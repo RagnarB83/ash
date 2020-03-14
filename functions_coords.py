ad64310574c0b168edfc230678d4ffa18ed32f54
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-03-14 21:39:41 +0100

    polembed interface

--- functions_coords.py
+++ functions_coords.py
@@ -369,18 +369,16 @@ def write_pdbfile_dummy(elems,coords,name, atomlabels):
     with open(name+'.pdb', 'w') as pfile:
         resnames=atomlabels
         #resnames=['QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'QM', 'HOH', 'HOH','HOH']
-        resids=[1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2]
-        pfile.write("ATOM      1  N   SER A   2      65.342  32.035  32.324  1.00  0.00           N\n")
-        for count,(el,c,resname, resid) in enumerate(zip(elems,coords, resnames, resids)):
-            print(count, el,c,resname, resid)
-            #line="{:>4}{:>7d}{:>3}{:>5}{:3d}{:8.3f}{:8.3f}{:8.3f}{:4}{:4}{:4}".format('ATOM', count+1, el,
-            #                                                                                resname, resid,
-            #                                                                                c[0], c[1], c[2],
-            #                                                                                '1.0', '0.00', 'X')
-            #
+        #resids=[1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2]
+        #Example:
+        #pfile.write("ATOM      1  N   SER A   2      65.342  32.035  32.324  1.00  0.00           N\n")
+        for count,(el,c,resname) in enumerate(zip(elems,coords, resnames)):
+            #print(count, el,c,resname)
+            #Dummy resid for everything
+            resid=1
+            #Using string format from: https://cupnet.net/pdb-format/
             line="{:6s}{:5d} {:^4s}{:1s}{:3s} {:1s}{:4d}{:1s}   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {:>2s}{:2s}".format(
                 'ATOM', count+1, el, '', resname, 'A', resid, '',    c[0], c[1], c[2], 1.0, 0.00, 'X', '')
-
             pfile.write(line+'\n')
     print("Wrote PDB file:", name+'.pdb')
 
