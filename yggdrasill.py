f7c62c313997deaf44a2e7b844c719d8a3cf80b1
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-03-17 16:39:42 +0000

    solvshell new module changes

--- yggdrasill.py
+++ yggdrasill.py
@@ -619,8 +619,6 @@ class PolEmbedTheory:
                     project.write_potential(system)
                     self.potfile=filename+'.pot'
                     print("Created potfile: ", self.potfile)
-                    print("exiting")
-                    exit()
                 elif self.pot_option=='TIP3P':
                     print("Pot option: TIP3P")
                     system = pyframe.MolecularSystem(input_file=file)
