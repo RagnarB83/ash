7412193f56fc042b818226df21c868da4297bd98
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-04-11 17:30:34 +0000

    fixing QM/MM bug. MM part was using fixed coordiantes

--- molcrys.py
+++ molcrys.py
@@ -33,7 +33,7 @@ def molcrys(cif_file='', fragmentobjects=[], theory=None, numcores=None, chargem
 
     print("Fragments defined:")
     for f in fragmentobjects:
-        print("Fragment:", f[.__dict__)
+        print("Fragment:", f.__dict__)
 
     origtime = time.time()
     currtime = time.time()
