79864fbe768e84ec2af71e8d8be958a82371b6f7
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-04-15 12:00:18 +0000

    Major reorganization of MM and LJ stuff

--- settings_yggdrasill.py
+++ settings_yggdrasill.py
@@ -1,13 +1,16 @@
 import yggdrasill
+import time
 #Defining global variables here: called as settings_yggdrasill.scale etc.
 def init():
     yggdrasill.print_yggdrasill_header()
     global scale
     global tol
     global conndepth
+    global init_time
     # Changing Scale, Tol, connectivity-recursivedepth
     # Scale=1.0, tol=0.1 and conndepth=10 should be good
     scale = 1.0
     tol = 0.1
     conndepth = 10
+    init_time=time.time()
     print(f"Setting global settings: \n Connectivity scale: {scale} and tol: {tol}")
