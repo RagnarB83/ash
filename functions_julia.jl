ccabaa7e33c6c4f61c313a3bb357eaa3d864fbf8
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-04-17 02:09:59 +0000

    even faster julia pairarray

--- functions_julia.jl
+++ functions_julia.jl
@@ -22,7 +22,39 @@ end
 #Key things for speed:
 # i:numatoms, j=i+1:numatoms
 # Using fast dict-lookup, simple double-if condition for qmatoms (was slowing things down a lot with all thing)
+
 function pairpot3(numatoms,atomtypes,LJpydict,qmatoms)
+    #Updating atom indices from 0 to 1 syntax
+    qmatoms=[i+1 for i in qmatoms]
+    #Convert Python dict to Julia dict with correct types
+    LJdict_jul=convert(Dict{Tuple{String,String},Array{Float64,1}}, LJpydict)
+    #println(typeof(LJdict_jul))
+    sigmaij=zeros(numatoms, numatoms)
+    epsij=zeros(numatoms, numatoms)
+for i in 1:numatoms
+    for j in i+1:numatoms
+        if i in qmatoms && j in qmatoms
+                continue
+        else
+           v = get(LJdict_jul, (atomtypes[i],atomtypes[j]), nothing)
+           if v !== nothing
+             sigmaij[i, j] = v[1]
+             epsij[i, j] =  v[2]
+           else
+             v = get(LJdict_jul, (atomtypes[j],atomtypes[i]), nothing)
+             if v !== nothing
+               sigmaij[i, j] = v[1]
+               epsij[i, j] =  v[2]
+             end
+           end
+	    end
+    end
+end
+return sigmaij,epsij
+end
+
+#Old version. More intuitive but slower
+function pairpot2(numatoms,atomtypes,LJpydict,qmatoms)
     #Updating atom indices from 0 to 1 syntax
     qmatoms=[i+1 for i in qmatoms]
     #Convert Python dict to Julia dict with correct types
