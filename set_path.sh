ea04187a5d8cd137d5364b9371f8d77e8a9d1b3a
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-04-27 22:16:54 +0000

    Add set_path_script

--- set_path.sh
+++ set_path.sh
@@ -1,8 +1,9 @@
-#
+#Getting path of this shell-script (resids in Yggdrasill main dir)
 SCRIPT=$(readlink -f "$0")
 yggdrasill_path=$(dirname "$SCRIPT")
-echo "yggdrasill_path : $yggdrasill_path"
 
-echo "Setting PATH and LD_LIBRARY_PATH for Yggdrasill"
+echo "Setting PYTHONPATH and LD_LIBRARY_PATH for Yggdrasill"
+#PYTHONPATH
 export PYTHONPATH=$yggdrasill_path:$PYTHONPATH
-export LD_LIBRARY_PATH=$yggpath/lib:$LD_LIBRARY_PATH
+#Setting LD_LIBRARY_PATH for lib dir
+export LD_LIBRARY_PATH=$yggdrasill_path/lib:$LD_LIBRARY_PATH
