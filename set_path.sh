fb9cda683b8063c18884b5dbe45168ca1b797c9a
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-04-27 22:05:44 +0000

    Add set_path_script

--- set_path.sh
+++ set_path.sh
@@ -1,11 +1,8 @@
-
-CUR_PATH=$(pwd)
-echo "CUR_PATH : $CUR_PATH"
+#
 SCRIPT=$(readlink -f "$0")
-echo "SCRIPT : $SCRIPT"
-yggpath=/home/bjornsson/YGGDRASILL-DEV_GIT/yggdrasill
+yggdrasill_path=$(dirname "$SCRIPT")
+echo "yggdrasill_path : $yggdrasill_path"
 
-echo "yggpath : $yggpath"
-
-export PYTHONPATH=$yggpath:$PYTHONPATH
+echo "Setting PATH and LD_LIBRARY_PATH for Yggdrasill"
+export PYTHONPATH=$yggdrasill_path:$PYTHONPATH
 export LD_LIBRARY_PATH=$yggpath/lib:$LD_LIBRARY_PATH
