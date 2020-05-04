c1759dba2f0873a308cbcc46d9ac8fa7ab602e0c
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-05-04 12:36:02 +0000

    knarr update

--- interface_knarr.py
+++ interface_knarr.py
@@ -26,7 +26,7 @@ import KNARRatom.atom
 #Knarr settings for path-generation, NEB and optimizer
 #These will be the reasonable defaults that can be overridden by special keywords in Yggdrasill NEB object
 path_parameters = {"METHOD": "DOUBLE", "INTERPOLATION": "IDPP", "NIMAGES": 6,
-              "INSERT_CONFIG": None, "IDPP_MAX_ITER": 2000,
+              "INSERT_CONFIG": None, "IDPP_MAX_ITER": 100,
               "IDPP_SPRINGCONST": 10.0, "IDPP_TIME_STEP": 0.01,
               "IDPP_MAX_MOVE": 0.1, "IDPP_MAX_F": 0.01, "IDPP_RMS_F": 0.005}
 
