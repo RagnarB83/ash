e8b06db4bff2f5f0e5892862f102e87cbea6a3e0
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-05-05 22:08:56 +0000

    printlevel in theories

--- yggdrasill.py
+++ yggdrasill.py
@@ -2439,13 +2439,13 @@ class Fragment:
             outfile.write("Num atoms: {}\n".format(self.numatoms))
             outfile.write("Energy: {}\n".format(self.energy))
             outfile.write("\n")
-            outfile.write(" Index    Atom         x             y             z          charge        fragment-type        atom-type\n")
-            outfile.write("-------------------------------------------------------------------------------------------------------------\n")
+            outfile.write(" Index    Atom         x                y                z             charge        fragment-type        atom-type\n")
+            outfile.write("--------------------------------------------------------------------------------------------------------------------\n")
             for at, el, coord, charge, label, atomtype in zip(self.atomlist, self.elems,self.coords,self.atomcharges, self.fragmenttype_labels, self.atomtypes):
                 line="{:>6} {:>6}  {:15.9f}  {:15.9f}  {:15.9f}  {:12.6f} {:12d} {:>21}\n".format(at, el,coord[0], coord[1], coord[2], charge, label, atomtype)
                 outfile.write(line)
             outfile.write(
-                "=============================================================================================================\n")
+                "====================================================================================================================\n")
             #outfile.write("elems: {}\n".format(self.elems))
             #outfile.write("coords: {}\n".format(self.coords))
             #outfile.write("list of masses: {}\n".format(self.list_of_masses))
