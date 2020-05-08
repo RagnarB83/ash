746a5875fc6a39d58ab110c91a29a4491cf22eef
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-05-08 10:09:40 +0000

    name change from yggdrasill to ash

--- ash.py
+++ ash.py
@@ -71,8 +71,9 @@ def print_ash_header():
 
     print(BC.WARNING,"----------------------------------------------------------------------------------",BC.END)
     print(BC.WARNING,"----------------------------------------------------------------------------------",BC.END)
-    print(BC.OKBLUE,ascii_banner3,BC.END)
+    #print(BC.OKBLUE,ascii_banner3,BC.END)
     print(BC.OKBLUE,ascii_banner2,BC.END)
+    print(BC.OKGREEN,ascii_banner2,BC.END)
     print(BC.WARNING,BC.BOLD,"ASH version", programversion,BC.END)
     print(BC.WARNING, "Git commit version: ", git_commit_number, BC.END)
     print(BC.WARNING,"A COMPCHEM AND QM/MM ENVIRONMENT", BC.END)
