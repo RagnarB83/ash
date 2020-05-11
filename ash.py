f3c26dfe0ee98544f9950fde0bad06fdaf5048f8
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-05-11 14:32:29 +0000

    numfreq fixes

--- ash.py
+++ ash.py
@@ -19,6 +19,43 @@ import subprocess as sp
 
 debugflag=False
 
+import sys
+import inspect
+
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects in bytes"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if hasattr(obj, '__dict__'):
+        for cls in obj.__class__.__mro__:
+            if '__dict__' in cls.__dict__:
+                d = cls.__dict__['__dict__']
+                if inspect.isgetsetdescriptor(d) or inspect.ismemberdescriptor(d):
+                    size += get_size(obj.__dict__, seen)
+                break
+    if isinstance(obj, dict):
+        size += sum((get_size(v, seen) for v in obj.values()))
+        size += sum((get_size(k, seen) for k in obj.keys()))
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum((get_size(i, seen) for i in obj))
+
+    if hasattr(obj, '__slots__'):  # can have __slots__ with __dict__
+        size += sum(get_size(getattr(obj, s), seen) for s in obj.__slots__ if hasattr(obj, s))
+
+    return size
+
+
+
+
+
 #Debug print. Behaves like print bug reads global debug var first
 def printdebug(string,var=''):
     global debugflag
@@ -256,11 +293,15 @@ def NumFreq(fragment=None, theory=None, npoint=1, displacement=0.0005, hessatoms
         #results = pool.map(displacement_run2, [[filelabel, numcoresQM, theory, label] for label,filelabel in zip(list_of_labels,list_of_filelabels)])
 
         #Reducing size of theory object
+        print("size of theory:", get_size(theory))
+        print("size of theory.coords:", get_size(theory.coords))
+        print("size of coords:", get_size(coords))
         theory.coords=[]
         theory.elems=[]
         theory.connectivity=[]
         print(theory)
-        print(theory.__dict__)
+        #print(theory.__dict__)
+        print("size of theory after del:", get_size(theory))
 
         #QMMM_xtb = QMMMTheory(fragment=Saddlepoint, qm_theory=xtbcalc, mm_theory=MMpart, actatoms=Centralmainfrag,
         #                      qmatoms=Centralmainfrag, embedding='Elstat', nprocs=numcores)
