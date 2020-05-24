9d87945be0aa6cd746a38d465d1fdd4a61768740
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-05-24 20:01:40 +0000

    PhotoElectron spectra

--- PES.py
+++ PES.py
@@ -13,6 +13,7 @@ import struct
 #from functions_coords import *
 from functions_ORCA import *
 from functions_general import *
+from ash import *
 
 class bcolors:
     HEADER = '\033[95m' ; OKBLUE = '\033[94m'; OKGREEN = '\033[92m'; WARNING = '\033[93m'; FAIL = '\033[91m'; ENDC = '\033[0m'; BOLD = '\033[1m'; UNDERLINE = '\033[4m'
