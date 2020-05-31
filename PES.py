b7a39e0e84d29dd894f968a8a38669dc4ab06152
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-05-31 18:31:48 +0000

    PES update: hopefully working now for open-shell/closed-shell

--- PES.py
+++ PES.py
@@ -23,6 +23,14 @@ class bcolors:
 eldict={'H':1,'He':2,'Li':3,'Be':4,'B':5,'C':6,'N':7,'O':8,'F':9,'Ne':10,'Na':11,'Mg':12,'Al':13,'Si':14,'P':15,'S':16,'Cl':17,'Ar':18,'K':19,'Ca':20,'Sc':21,'Ti':22,'V':23,'Cr':24,'Mn':25,'Fe':26,'Co':27,'Ni':28,'Cu':29,'Zn':30,'Ga':31,'Ge':32,'As':33,'Se':34,'Br':35,'Kr':36,'Mo':42,'W':74,'Ru':44,'I':53}
 
 
+#Cleanup function. Delete MO-files, det files, etc.
+def cleanup():
+    files=['AO_overl', 'dets_final', 'dets_init', 'memlog', 'wfovl.inp', 'wfovl.out', 'mos_final', 'mos_init']
+    for file in files:
+        try:
+            os.remove(file)
+        except:
+            pass
 
 #Calculate nuclear charge from XYZ-file
 def nuccharge(file):
