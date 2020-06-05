ec4b9b3626487c75d96719ec0fa9359aca6ad81f
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-06-05 10:16:48 +0000

    PES: printing

--- PES.py
+++ PES.py
@@ -1107,7 +1107,7 @@ def PhotoElectronSpectrum(theory=None, fragment=None, InitialState_charge=None, 
         print("Initial state:")
         print("{:<10} {:<10} {:>20}".format("State no.", "Mult", "TotalE"))
         #{:<20} {:>20.12f}
-        print("{:>6} {{:>6} {{:17}".format(0, stateI.mult, stateI.energy))
+        print("{:>6} {:>6} {:17}".format(0, stateI.mult, stateI.energy))
         print("")
         print("Final ionized states:")
         print("{:>6} {:>6} {:17} {:8} {:10} {:>5}".format("State no.", "Mult", "TotalE", "IE", "Dyson-norm", "State-type"))
