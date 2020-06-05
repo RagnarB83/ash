505722d3972999d09d3c6349f7136792ce8226ad
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-06-05 10:15:47 +0000

    PES: printing

--- PES.py
+++ PES.py
@@ -1107,10 +1107,10 @@ def PhotoElectronSpectrum(theory=None, fragment=None, InitialState_charge=None, 
         print("Initial state:")
         print("{:<10} {:<10} {:>20}".format("State no.", "Mult", "TotalE"))
         #{:<20} {:>20.12f}
-        print("{:<10d} {:<10d} {:>20.12f}".format(0, stateI.mult, stateI.energy))
+        print("{:>6} {{:>6} {{:17}".format(0, stateI.mult, stateI.energy))
         print("")
         print("Final ionized states:")
-        print("{:>6} {:>6} {:17.11} {:8.3} {:10.5} {:>5}".format("State no.", "Mult", "TotalE", "IE", "Dyson-norm", "State-type"))
+        print("{:>6} {:>6} {:17} {:8} {:10} {:>5}".format("State no.", "Mult", "TotalE", "IE", "Dyson-norm", "State-type"))
         for i, (E, IE, dys) in enumerate(zip(Finalionstates,FinalIPs,finaldysonnorms)):
             if i == 0:
                 stype='SCF'
