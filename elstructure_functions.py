791755c96def878a8e7a9b80bbe4a693455bfe0a
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-06-12 23:18:21 +0000

    PES diffdens option

--- elstructure_functions.py
+++ elstructure_functions.py
@@ -210,18 +210,19 @@ def write_cube_diff(numatoms,orgx,orgy,orgz, nx,dx,ny,dy,nz,dz,elems, molcoords,
             print("v2: ", v2)
             diff = [i-j for i,j in zip(v1,v2)]
             print("diff:", diff)
+
             if len(v1) == 6:
-                file.write("   {:9.6f}   {:9.6f}   {:9.6f}   {:9.6f}   {:9.6f}   {:9.6f}\n".format(diff[0],diff[1],diff[2],diff[3],diff[4],diff[5]))
+                file.write("   {:.6e}   {:.6e}   {:.6e}   {:.6f}   {:.6e}   {:.6e}\n".format(diff[0],diff[1],diff[2],diff[3],diff[4],diff[5]))
             elif len(v1) == 5:
-                file.write("   {:9.6f}   {:9.6f}   {:9.6f}   {:9.6f}   {:9.6f}\n".format(diff[0],diff[1],diff[2],diff[3],diff[4]))
+                file.write("   {:.6e}   {:.6e}   {:.6e}   {:.6e}   {:.6e}\n".format(diff[0],diff[1],diff[2],diff[3],diff[4]))
             elif len(v1) == 4:
-                file.write("   {:9.6f}   {:9.6f}   {:9.6f}   {:9.6f}\n".format(diff[0],diff[1],diff[2],diff[3]))
+                file.write("   {:.6e}   {:.6e}   {:.6e}   {:.6e}\n".format(diff[0],diff[1],diff[2],diff[3]))
             elif len(v1) == 3:
-                file.write("   {:9.6f}   {:9.6f}   {:9.6f}\n".format(diff[0],diff[1],diff[2]))
+                file.write("   {:.6e}   {:.6e}   {:.6e}\n".format(diff[0],diff[1],diff[2]))
             elif len(v1) == 2:
-                file.write("   {:9.6f}   {:9.6f}\n".format(diff[0],diff[1]))
+                file.write("   {:.6e}   {:.6e}\n".format(diff[0],diff[1]))
             elif len(v1) == 1:
-                file.write("   {:9.6f}\n".format(diff[0]))
+                file.write("   {:.6e}\n".format(diff[0]))
 
 
 #Read cubefile. Grabs coords. Calculates density if MO
