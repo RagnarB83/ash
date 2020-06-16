740f890fe9d4e7168293b35d7c979cfed0175ca4
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-06-16 13:05:58 +0000

    crest interface

--- ash.py
+++ ash.py
@@ -3300,12 +3300,10 @@ def call_crest(fragment=None, xtbmethod=None, crestdir=None,charge=None, mult=No
 
     uhf=mult-1
     #GBSA solvation or not
-    if solvent is  None:
-        process = sp.run([crestdir + '/crest', 'initial.xyz', '-gfn'+str(xtbflag), '-ewin', str(energywindow), '-chrg', str(charge),
-        '-uhf', str(mult-1)], check=True, stdout=sp.STDOUT, stderr=sp.STDOUT, universal_newlines=True)
+    if solvent is None:
+        process = sp.run([crestdir + '/crest', 'initial.xyz', '-gfn'+str(xtbflag), '-ewin', str(energywindow), '-chrg', str(charge), '-uhf', str(mult-1)], check=True, stdout=sp.STDOUT, stderr=sp.STDOUT, universal_newlines=True)
     else:
-        process = sp.run([crestdir + '/crest', 'initial.xyz', '-gfn'+str(xtbflag), '-ewin', str(energywindow), '-gbsa', str(solvent),
-        '-chrg', str(charge), '-uhf', str(mult-1)], check=True, stdout=sp.STDOUT, stderr=sp.STDOUT, universal_newlines=True)
+        process = sp.run([crestdir + '/crest', 'initial.xyz', '-gfn'+str(xtbflag), '-ewin', str(energywindow), '-gbsa', str(solvent), '-chrg', str(charge), '-uhf', str(mult-1)], check=True, stdout=sp.STDOUT, stderr=sp.STDOUT, universal_newlines=True)
 
     os.chdir('..')
 
