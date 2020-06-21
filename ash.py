9f5640471273521e40952f2d68980df6681592d9
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-06-21 18:34:55 +0000

    Psi4 improvements

--- ash.py
+++ ash.py
@@ -2395,9 +2395,11 @@ class Psi4Theory:
                     inputfile.write('\n')
 
                 if Grad==True:
-                    inputfile.write('scf_energy, wfn = gradient(\'scf\', dft_functional=\'{}\', return_wfn=True)\n'.format(self.psi4functional))
+                    #inputfile.write('scf_energy, wfn = gradient(\'scf\', dft_functional=\'{}\', return_wfn=True)\n'.format(self.psi4functional))
+                    inputfile.write("energy, wfn = gradient({}, return_wfn=True)\n".format(self.psi4method))
                 else:
-                    inputfile.write('scf_energy, wfn = energy(\'scf\', dft_functional=\'{}\', return_wfn=True)\n'.format(self.psi4functional))
+                    #inputfile.write('scf_energy, wfn = energy(\'scf\', dft_functional=\'{}\', return_wfn=True)\n'.format(self.psi4functional))
+                    inputfile.write('energy, wfn = energy({}, return_wfn=True)\n'.format(self.psi4method))
                     inputfile.write('\n')
 
             print("Running inputfile:", self.label+'.inp')
