34734db92f77a0b323a99179b58ddc86f91d3b79
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2020-08-07 17:01:56 +0000

    test

--- functions_ORCA.py
+++ functions_ORCA.py
@@ -815,7 +815,6 @@ def run_orca_plot(orcadir, filename, option, gridvalue=40,densityfilename=None, 
     elif option=='mo':
         print(mo_operator)
         print(mo_number)
-        exit()
         p = sp.run([orcadir + '/orca_plot', filename, '-i'], stdout=sp.PIPE,
                        input='5\n7\n4\n{}\n3\n{}\n2\n{}\n10\n11\n\n'.format(gridvalue,mo_operator,mo_number), encoding='ascii')
     #If plotting CIS/TDDFT density then we tell orca_plot explicity.
