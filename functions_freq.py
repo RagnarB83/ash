663cc658dd0f921ce368c49f3f846e90b35dc11f
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2021-01-16 17:58:13 +0000

    bugfix

--- functions_freq.py
+++ functions_freq.py
@@ -387,9 +387,9 @@ def thermochemcalc(vfreq,atoms,fragment, multiplicity, temp=298.15,pressure=1.0)
 
         #Rotational temperatures
         #k_b_JK or R_JK
-        rot_temps_x=constants.h_planck**2 / (8*math.pi**2 * constants.R_gasconst_JK * I[0])
-        rot_temps_y=constants.h_planck**2 / (8*math.pi**2 * constants.R_gasconst_JK * I[1])
-        rot_temps_z=constants.h_planck**2 / (8*math.pi**2 * constants.R_gasconst_JK * I[2])
+        rot_temps_x=constants.h_planck**2 / (8*math.pi**2 * constants.k_b_JK * I[0])
+        rot_temps_y=constants.h_planck**2 / (8*math.pi**2 * constants.k_b_JK * I[1])
+        rot_temps_z=constants.h_planck**2 / (8*math.pi**2 * constants.k_b_JK * I[2])
         print("Rotational temperatures: {}, {}, {} K".format(rot_temps_x,rot_temps_y,rot_temps_z))
         #Rotational constants
         rotconstants = calc_rotational_constants(fragment, printlevel=1)
