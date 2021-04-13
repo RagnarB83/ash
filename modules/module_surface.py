15e9e42e4581ced5aee0e976f060d297a42e36e1
RagnarB83 <ragnar.bjornsson@gmail.com>
2021-04-13 12:38:11 +0200

    update2

--- modules/module_surface.py
+++ modules/module_surface.py
@@ -12,7 +12,7 @@ import shutil
 import copy
 
 import ash
-from functions_general import frange, BC
+from functions_general import frange, BC, print_line_with_mainheader,print_line_with_subheader1
 import interface_geometric
 from module_freq import calc_rotational_constants
 import functions_parallel
