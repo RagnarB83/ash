6b81ce51700e35e6f67b0da3ffc8e93e74c98609
RagnarB83 <ragnar.bjornsson@gmail.com>
2021-04-25 18:49:53 +0200

    removed debug printing

--- functions/functions_elstructure.py
+++ functions/functions_elstructure.py
@@ -935,9 +935,6 @@ def select_space_from_occupations(occlist, selection_thresholds=[1.98,0.02]):
     upper_threshold=selection_thresholds[0]
     lower_threshold=selection_thresholds[1]
     welloccorbs=[i for i in occlist if i < upper_threshold and i > lower_threshold]
-    print("welloccorbs:", welloccorbs)
     numelectrons=round(sum(welloccorbs))
-    print("numelectrons:", numelectrons)
     numorbitals=len(welloccorbs)
-    print("numorbitals:", numorbitals)
     return [numelectrons,numorbitals]
