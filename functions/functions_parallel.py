f0f2f0accdab59da62542446092ec38e23e07f1b
RagnarB83 <ragnar.bjornsson@gmail.com>
2021-06-29 16:10:46 +0200

    cleanup

--- functions/functions_parallel.py
+++ functions/functions_parallel.py
@@ -339,6 +339,7 @@ def Singlepoint_parallel(fragments=None, fragmentfiles=None, theories=None, numc
     pool.close()
     pool.join()
     print("results:", results)
+    print(results.get())
     energy_dict = {result[0]: result[1] for result in results}
     print("energy_dict:", energy_dict)
 
