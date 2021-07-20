51c21ef0e289c42f83a0e47a0677ca8425ec1788
RagnarB83 <ragnar.bjornsson@gmail.com>
2021-07-20 12:43:28 +0200

    example update

--- examples/OpenMM-protein-preparation/lysozyme/protein-prep-MM_MD-andQMMM.py
+++ examples/OpenMM-protein-preparation/lysozyme/protein-prep-MM_MD-andQMMM.py
@@ -6,9 +6,14 @@ numcores=4
 #Original raw PDB-file (no hydrogens, nosolvent). Lysozyme example
 pdbfile="1aki.pdb"
 
+
+#Defining residues with special user-wanted protonation states
+residue_variants={}
+
 #Setting up new system, adding hydrogens, solvent, ions and defining forcefield, topology
-forcefield, topology, ashfragment = OpenMM_Modeller(pdbfile=pdbfile, forcefield='CHARMM36', watermodel="tip3p", pH=7.0, solvent_padding=10.0, ionicstrength=0.1, iontype="Na+")
-# extraxmlfile="specialresidue.xml",
+forcefield, topology, ashfragment = OpenMM_Modeller(pdbfile=pdbfile, forcefield='CHARMM36', watermodel="tip3p", pH=7.0, 
+    solvent_padding=10.0, ionicstrength=0.1, iontype="Na+", residue_variants=residue_variants)
+
 #Creating new OpenMM object from forcefield, topology and and fragment
 openmmobject =OpenMMTheory(platform='CPU', numcores=numcores, Modeller=True, forcefield=forcefield, topology=topology,
                  pdbfile=None, do_energy_decomposition=True, periodic=True,
