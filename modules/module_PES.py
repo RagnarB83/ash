f2ea39900cb2ee4854af9687fa33075cde27b19d
RagnarB83 <ragnar.bjornsson@gmail.com>
2021-07-22 00:42:04 +0200

    PES, ORCA5 fix

--- modules/module_PES.py
+++ modules/module_PES.py
@@ -257,7 +257,9 @@ def get_dets_from_single(logfile,restr,gscharge,gsmult,totnuccharge,frozencore):
     data=readfile(logfile)
     infos={}
     for iline,line in enumerate(data):
-      if '# of contracted basis functions' in line:
+      #print("line:", line)
+      #if '# of contracted basis functions' in line:
+      if 'Number of basis functions                   ...' in line:
         infos['nbsuse']=int(line.split()[-1])
       if 'Orbital ranges used for CIS calculation:' in line:
         s=data[iline+1].replace('.',' ').split()
@@ -271,7 +273,6 @@ def get_dets_from_single(logfile,restr,gscharge,gsmult,totnuccharge,frozencore):
           s=data[iline+2].replace('.',' ').split()
           infos['NOB']=int(s[4])-int(s[3])+1
           infos['NVB']=int(s[7])-int(s[6])+1
-
     if not 'NOA' in infos:
       charge=gscharge
       #charge=QMin['chargemap'][gsmult]
@@ -336,7 +337,8 @@ def get_dets_from_cis(logfile,cisfilename,restr,mults,gscharge,gsmult,totnucchar
     data=readfile(logfile)
     infos={}
     for iline,line in enumerate(data):
-      if '# of contracted basis functions' in line:
+      #if '# of contracted basis functions' in line:
+      if 'Number of basis functions                   ...' in line:
         infos['nbsuse']=int(line.split()[-1])
       if 'Orbital ranges used for CIS calculation:' in line:
         s=data[iline+1].replace('.',' ').split()
