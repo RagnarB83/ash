29035ddecf117cd6fd3a0b959131e74458a9223b
RagnarB83 <ragnar.bjornsson@gmail.com>
2021-08-13 14:15:24 +0200

    QMregionfragexpand global function

--- ash.py
+++ ash.py
@@ -34,7 +34,7 @@ from functions.functions_general import blankline,BC,listdiff,print_time_rel,pri
 # Fragment class and coordinate functions
 import modules.module_coords
 from modules.module_coords import get_molecules_from_trajectory,eldict_covrad,write_pdbfile,Fragment,read_xyzfile,write_xyzfile,make_cluster_from_box, read_ambercoordinates, read_gromacsfile
-from modules.module_coords import remove_atoms_from_system_CHARMM,add_atoms_to_system_CHARMM, getwaterconstraintslist
+from modules.module_coords import remove_atoms_from_system_CHARMM,add_atoms_to_system_CHARMM, getwaterconstraintslist, QMregionfragexpand
 #Singlepoint
 import modules.module_singlepoint
 from modules.module_singlepoint import Singlepoint,ZeroTheory
