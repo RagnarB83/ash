523cc3df2a16c3ab72382bf0ae7d7062987f8bda
Ragnar Bjornsson <ragnar.bjornsson@gmail.com>
2021-11-12 16:42:47 +0100

    tutorial update

--- examples/OpenMM_Modeller-setups/rubredoxin/1-modelsetup_advanced.py
+++ examples/OpenMM_Modeller-setups/rubredoxin/1-modelsetup_advanced.py
@@ -13,4 +13,4 @@ residue_variants={5:'CYX',8:'CYX',38:'CYX',41:'CYX'}
 # Setting up system via Modeller
 OpenMM_Modeller(pdbfile=pdbfile, forcefield='CHARMM36',
     extraxmlfile=extraxmlfile, watermodel="tip3p", pH=7.0, solvent_padding=10.0,
-    ionicstrength=0.1, pos_iontype='Na+', neg_iontyp='Cl-', residue_variants=residue_variants)
+    ionicstrength=0.1, pos_iontype='Na+', neg_iontype='Cl-', residue_variants=residue_variants)
--- interfaces/interface_OpenMM.py
+++ interfaces/interface_OpenMM.py
@@ -2105,11 +2105,11 @@ def OpenMM_Modeller(pdbfile=None, forcefield=None, xmlfile=None, waterxmlfile=No
     print("finalsystem.xyz   (XYZ coordinate file)")
     print("{}   (System XML file)".format(systemxmlfile))
     print(BC.OKGREEN,"\n\n OpenMM_Modeller done! System has been fully set up\n",BC.END)
-    print("To use this system setup to define a future OpenMMTheory object for this system you can either do:\n")
+    print("To use this system setup to define a future OpenMMTheory object you can either do:\n")
     print(BC.OKMAGENTA,"1. Use full system XML-file:\n",BC.END, \
-        "omm = OpenMMTheory(xmlsystemfile=\"system_full.xml\", pdbfile=\"finalsystem.pdb\", periodic=True)",BC.END)
+        "omm = OpenMMTheory(xmlsystemfile=\"system_full.xml\", pdbfile=\"finalsystem.pdb\", periodic=True)\n",BC.END)
 
-    print(BC.OKMAGENTA,"\n2. Define using separate forcefield XML files:",BC.END)
+    print(BC.OKMAGENTA,"2. Define using separate forcefield XML files:",BC.END)
     if extraxmlfile is None:
         print("omm = OpenMMTheory(xmlfiles=[{}, {}], pdbfile=\"finalsystem.pdb\", periodic=True)".format(xmlfile,waterxmlfile),BC.END)
     else:
