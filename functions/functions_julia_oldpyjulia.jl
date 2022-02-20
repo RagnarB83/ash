514820ae1df191a8c0f921ee59cc2028ef519020
RagnarB83 <ragnar.bjornsson@gmail.com>
2022-02-20 12:15:40 +0100

    pyjulia fix hopefully

--- functions/functions_julia_oldpyjulia.jl
+++ functions/functions_julia_oldpyjulia.jl
@@ -83,14 +83,6 @@ end
 # JULIA functions not intended to be called by Python:
 ########################################################
 
-#Connectivity (fraglists) for whole fragment
-function calc_connectivity(coords,elems,conndepth,scale, tol,eldict_covrad)
-    println("1 type eldict_covrad", typeof(eldict_covrad))
-	#0-index based atomlist
-	atomlist=[0:length(elems)-1;]
-	return calc_fraglist_for_atoms(atomlist,coords, elems, conndepth, scale, tol,eldict_covrad)
-end
-
 
 #Get fraglist for list of atoms (called by molcrys directly). Using 0-based indexing until get_conn_atoms
 function calc_fraglist_for_atoms(atomlist,coords, elems, conndepth, scale, tol,eldict_covrad)
