399b8981100079e24990da18d3db6dc38040c49f
RagnarB83 <ragnar.bjornsson@gmail.com>
2022-11-16 00:42:46 +0100

    new PySCFTheory interface: - cleanup - distinguish between pure SCF and post-SCF - better DFT support - coupled cluster: R/U CCSD and CCSD(T) - frozen core setting - frozen virtuals - Auto Frozen natural orbital CC with MP2-nat orbs (still ironing out the kinks)

--- interfaces/interface_pyscf.py
+++ interfaces/interface_pyscf.py
@@ -1,23 +1,40 @@
 import time
 
-from ash.functions.functions_general import ashexit, BC,print_time_rel
+from ash.functions.functions_general import ashexit, BC,print_time_rel, print_line_with_mainheader
 import ash.modules.module_coords
+import scipy 
 
 #PySCF Theory object.
 #PySCF runmode: Library only
 # PE: Polarizable embedding (CPPE). Not completely active in PySCF 1.7.1. Bugfix required I think
 class PySCFTheory:
-    def __init__(self, printsetting=False, printlevel=2, pyscfbasis='', pyscffunctional='',
-                 pe=False, potfile='', filename='pyscf', pyscfmemory=3100, numcores=1):
-
-        #Indicate that this is a QMtheory
+    def __init__(self, printsetting=False, printlevel=2, scf_type=None, basis=None, functional=None,
+                 pe=False, potfile='', CC=False, filename='pyscf', pyscfmemory=3100, numcores=1, conv_tol=1e-8,
+                 verbose_setting=4, gridlevel=5, frozen_core_setting='Auto', CCmethod=None, CC_direct=False,
+                 frozen_virtuals=None, FNO=False, FNO_thresh=None):
         self.theorytype="QM"
-        
+        print_line_with_mainheader("PySCFTheory initialization")
+        #Exit early if no SCF-type
+        if scf_type is None:
+            print("Error: You must select an scf_type, e.g. 'RHF', 'UHF', 'RKS', 'UKS'")
+            ashexit()
+        if functional is not None:
+            print(f"Functional keyword: {functional} chosen. DFT is on!")
+            if scf_type == 'RHF':
+                print("Changing RHF to RKS")
+                scf_type='RKS'
+            if scf_type == 'UHF':
+                print("Changing UHF to UKS")
+                scf_type='UKS'
+        else:
+            if scf_type == 'RKS' or scf_type == 'UKS':
+                print("Error: RKS/UKS chosen but no functional. Exiting")
+                ashexit()
+        if CC is True and CCmethod is None:
+            print("Error: Need to choose CCmethod, e.g. 'CCSD', 'CCSD(T)")
+            ashexit()
         #Printlevel
         self.printlevel=printlevel
-
-        self.numcores=numcores
-
         self.pyscfmemory=pyscfmemory
         self.filename=filename
         self.printsetting=printsetting
@@ -26,11 +43,93 @@ class PySCFTheory:
         #Potfile from user or passed on via QM/MM Theory object ?
         self.potfile=potfile
 
-        self.pyscfbasis=pyscfbasis
-        self.pyscffunctional=pyscffunctional
+        #
+        self.scf_type=scf_type
+        self.basis=basis
+        self.functional=functional
+        self.CC=CC
+        self.CCmethod=CCmethod
+        self.CC_direct=CC_direct
+        self.FNO=FNO
+        self.FNO_thresh=FNO_thresh
+        self.frozen_core_setting=frozen_core_setting
+        self.frozen_virtuals=frozen_virtuals
+        self.gridlevel=gridlevel
+
+        self.conv_tol=conv_tol
+        self.verbose_setting=verbose_setting
+
+        self.load_pyscf()
+        self.set_numcores(numcores)
+        #PySCF scratch dir. Todo: Need to adapt
+        #print("Setting PySCF scratchdir to ", os.getcwd())
+
+        #Print the options
+        print("SCF-type:", self.scf_type)
+        print("conv_tol:", self.conv_tol)
+        print("Grid level:", self.gridlevel)
+        print("verbose_setting:", self.verbose_setting)
+        print("Basis:", self.basis)
+        print("Functional:", self.functional)
+        print("Coupled cluster:", self.CC)
+        print("CC method:", self.CCmethod)
+        print("CC direct:", self.CC_direct)
+        print("FNO-CC:", self.FNO)
+        print("FNO_thresh:", self.FNO_thresh)
+
+        print("Frozen_core_setting:", self.frozen_core_setting)
+        print("Frozen_virtual orbitals:",self.frozen_virtuals)
+        print("Polarizable embedding:", self.pe)
+        #TODO: Restart settings for PySCF
+
+
+    def determine_frozen_core(self,elems):
+        print("Determining frozen core")
+        #Main elements 
+        FC_elems={'H':0,'He':0,'Li':0,'Be':0,'B':2,'C':2,'N':2,'O':2,'F':2,'Ne':2,
+        'Na':2,'Mg':2,'Al':10,'Si':10,'P':10,'S':10,'Cl':10,'Ar':10,
+        'K':10,'Ca':10,'Sc':10,'Ti':10,'V':10,'Cr':10,'Mn':10,'Fe':10,'Co':10,'Ni':10,'Cu':10,'Zn':10,
+        'Ga':18,'Ge':18,'As':18,'Se':18, 'Br':18, 'Kr':18}
+        #NOTE: To be updated for 4d TM row etc
+        num_el=0
+        for el in elems:
+            num_el+=FC_elems[el]
+        self.frozen_core_el=num_el
+        self.frozen_core_orbs=int(num_el/2)
+        print("Total frozen electrons in system:", self.frozen_core_el)
+        print("Total frozen orbitals in system:", self.frozen_core_orbs)
+        #Create list of frozen orbital indices
+        self.frozen_core_orbital_indices=[i for i in range(0,self.frozen_core_orbs)]
+        print("List of frozen orbital indices:", self.frozen_core_orbital_indices)
+
+
+    def load_pyscf(self):
+        try:
+            import pyscf
+        except:
+            print(BC.FAIL, "Problem importing pyscf. Make sure pyscf has been installed: pip install pyscf", BC.END)
+            ashexit(code=9)
+        self.pyscf=pyscf
+        from pyscf.tools import molden
+        self.pyscf_molden=molden
+        if self.CC is True:
+            from pyscf import cc
+            self.pyscf_cc=cc
+            from pyscf.mp.dfump2_native import DFMP2
+            self.pyscf_dmp2=DFMP2
+
+        #TODO: Needs to be revisited
+        if self.pe==True:
+            #import pyscf.solvent as solvent
+            #from pyscf.solvent import pol_embed
+            import cppe
+
     #Set numcores method
     def set_numcores(self,numcores):
         self.numcores=numcores
+        print("Setting numcores to: ", self.numcores)
+        #Controlling OpenMP parallelization.
+        self.pyscf.lib.num_threads(numcores)
     #Cleanup after run.
     def cleanup(self):
         print("Cleaning up old PySCF files")
@@ -45,10 +144,6 @@ class PySCFTheory:
             charge=None, mult=None ):
 
         module_init_time=time.time()
-
-        if numcores==None:
-            numcores=self.numcores
-
 
         print(BC.OKBLUE,BC.BOLD, "------------RUNNING PYSCF INTERFACE-------------", BC.END)
 
@@ -78,40 +173,25 @@ class PySCFTheory:
             else:
                 qm_elems = elems
 
-
-        try:
-            import pyscf
-        except:
-            print(BC.FAIL, "Problem importing pyscf. Make sure pyscf has been installed: pip install pyscf", BC.END)
-            ashexit(code=9)
-        #PySCF scratch dir. Todo: Need to adapt
-        #print("Setting PySCF scratchdir to ", os.getcwd())
-
-        from pyscf import gto
-        from pyscf import scf
-        from pyscf import lib
-        from pyscf.dft import xcfun
-        if self.pe==True:
-            import pyscf.solvent as solvent
-            from pyscf.solvent import pol_embed
-            import cppe
-
-        #Defining mol object
-        mol = gto.Mole()
+        #MOL OBJECT
+        #NOTE: Simplify
+        #Defining pyscf mol object and populating 
+        mol = self.pyscf.gto.Mole()
         #Not very verbose system printing
         mol.verbose = 3
         coords_string=ash.modules.module_coords.create_coords_string(qm_elems,current_coords)
         mol.atom = coords_string
-        mol.symmetry = 1
-        mol.charge = charge
-        mol.spin = mult-1
+        mol.symmetry = 1;  mol.charge = charge; mol.spin = mult-1
         #PYSCF basis object: https://sunqm.github.io/pyscf/tutorial.html
         #Object can be string ('def2-SVP') or a dict with element-specific keys and values
-        mol.basis=self.pyscfbasis
+        mol.basis=self.basis
         #Memory settings
         mol.max_memory = self.pyscfmemory
         #BUILD mol object
         mol.build()
+        ###########
+
+        #Polarizable embedding option
         if self.pe==True:
             print(BC.OKGREEN, "Polarizable Embedding Option On! Using CPPE module inside PySCF", BC.END)
             print(BC.WARNING, "Potfile: ", self.potfile, BC.END)
@@ -123,57 +203,179 @@ class PySCFTheory:
                     ashexit()
             except:
                 ashexit()
-
             # TODO: Adapt to RKS vs. UKS etc.
-            mf = solvent.PE(scf.RKS(mol), self.potfile)
+            mf = self.pyscf.solvent.PE(self.pyscf.scf.RKS(mol), self.potfile)
+        #Regular job
         else:
-
             if PC is True:
                 # QM/MM pointcharge embedding
                 #mf = mm_charge(dft.RKS(mol), [(0.5, 0.6, 0.8)], MMcharges)
-                mf = mm_charge(dft.RKS(mol), current_MM_coords, MMcharges)
-
+                if self.scf_type == 'RKS':
+                    mf = self.pyscf.qmmm.mm_charge(dft.RKS(mol), current_MM_coords, MMcharges)
+                else:
+                    print("Error. scf_type other than RKS and PC True not ready")
+                    ashexit()
             else:
-                #TODO: Adapt to RKS vs. UKS etc.
-                mf = scf.RKS(mol)
-                #Verbose printing. TODO: put somewhere else
-            mf.verbose=4
-
+                if self.scf_type == 'RKS':
+                    mf = self.pyscf.scf.RKS(mol)
+                elif self.scf_type == 'UKS':
+                    mf = self.pyscf.scf.UKS(mol)
+                elif self.scf_type == 'RHF':
+                    mf = self.pyscf.scf.RHF(mol)
+                elif self.scf_type == 'UHF':
+                    mf = self.pyscf.scf.UHF(mol)
 
         #Printing settings.
         if self.printsetting==True:
             print("Printsetting = True. Printing output to stdout...")
             #np.set_printoptions(linewidth=500) TODO: not sure
         else:
-            print("Printsetting = False. Printing to:", self.filename+'.out' )
-            mf.stdout = open(self.filename+'.out', 'w')
+            print("Printsetting = False. Printing to:", self.filename )
+            mf.stdout = open(self.filename, 'w')
 
+        #DFT
+        if self.functional is not None:
+            #Setting functional
+            mf.xc = self.functional
+            #TODO: libxc vs. xcfun interface control here
+            #mf._numint.libxc = xcfun
 
-        #TODO: Restart settings for PySCF
+            #Grid setting
+            mf.grids.level = self.gridlevel
 
-        #Controlling OpenMP parallelization.
-        lib.num_threads(numcores)
-
-        #Setting functional
-        mf.xc = self.pyscffunctional
-        #TODO: libxc vs. xcfun interface control here
-        #mf._numint.libxc = xcfun
-
-        mf.conv_tol = 1e-8
-        #Control printing here. TOdo: make variable
-        mf.verbose = 4
-
-
-
-        #RUN ENERGY job. mf object should have been wrapped by PE or PC here
-        result = mf.run()
-        self.energy = result.e_tot
-        print("SCF energy components:", result.scf_summary)
         
-        #if self.pe==True:
-        #    print(mf._pol_embed.cppe_state.summary_string)
+        mf.conv_tol = self.conv_tol
+        #Control printing here. TOdo: make variable
+        mf.verbose = self.verbose_setting
 
+        #FROZEN ORBITALS in CC
+        if self.CC:
+            #Frozen-core settings
+            if self.frozen_core_setting == 'Auto':
+                self.determine_frozen_core(elems)
+            elif self.frozen_core_setting == None or self.frozen_core_setting == 'None':
+                print("Warning: No core-orbitals will be frozen in the CC calculation.")
+                self.frozen_core_orbital_indices=None
+            else:
+                print("Manual user frozen core:", frozen_orbs)
+                self.frozen_core_orbital_indices=self.frozen_core_setting
+            #Optional frozen virtuals also
+            if self.frozen_virtuals is not None:
+                print(f"Frozen virtuals option active. Will freeze orbitals {self.frozen_virtuals}.")
+                self.frozen_orbital_indices = self.frozen_core_orbital_indices + self.frozen_virtuals
+            else:
+                self.frozen_orbital_indices=self.frozen_core_orbital_indices
+            print("Final frozen-orbital list (core and virtuals):", self.frozen_orbital_indices)
+        ############
+        #RUNNING
+        ############
+        print()
+        #COUPLED CLUSTER RUN
+        if self.CC is True:
+            print("Now running CC job")
+
+
+
+            #SCF-part
+            print("First running SCF")
+            scf_result = mf.run()
+            print("SCF energy:", scf_result.e_tot)
+            print("SCF energy components:", scf_result.scf_summary)
+            if self.scf_type == 'RHF' or self.scf_type == 'RKS':
+                num_scf_orbitals_alpha=len(scf_result.mo_occ)
+                print("Total num. orbitals:", num_scf_orbitals_alpha)
+            else:
+                num_scf_orbitals_alpha=len(scf_result.mo_occ[0])
+                print("Total num. orbitals:", num_scf_orbitals_alpha)
+
+            #Default MO coefficients None (unless MP2natorbs option below)
+            mo_coefficients=None
+
+            #Optional MP2 natural orbitals
+            if self.FNO is True:
+                print("FNO is True")
+                print("MP2 natural orbitals on!")
+                print("Will calculate MP2 natural orbitals use as input in CC job")
+                #ALTERNATIVE: https://github.com/pyscf/pyscf/issues/466
+                #pt = self.pyscf.mp.MP2(mf)
+                #mp2_E, t2 = pt.kernel(mf.mo_energy, mf.mo_coeff)
+                # form the one body density matrix
+                #rdm1 = pt.make_rdm1()
+                #print("rdm1:", rdm1)
+                # diagonalize to yield the NOs and NO occupation #s
+                #occ, no = scipy.linalg.eigh(rdm1)
+                # eigenvalues are sorted in ascending order so reorder
+                #occ = occ[::-1]
+                #no = no[:, ::-1]
+                #print("no:", no)
+                #ashexit()
+                # MP2 natural occupation numbers and natural orbitals
+                natocc, natorb = self.pyscf_dmp2(mf.to_uhf()).make_natorbs()
+                print("MP2 natural orbital occupations:", natocc)
+                print("Writing MP2 natural orbitals to disk as Molden file")
+                self.pyscf_molden.from_mo(mol, 'pyscf_mp2nat.molden', natorb, occ=natocc)
+
+
+                #Choosing MO-coeffients to be
+                if self.scf_type == 'RHF' or self.scf_type == 'RKS':
+                    mo_coefficients=natorb              
+                else:
+                    mo_coefficients=[natorb,natorb]
+
+                #Optional natorb truncation if FNO_thresh is chosen
+                if self.FNO_thresh is not None:
+                    print("FNO thresh option chosen:", self.FNO_thresh)
+                    num_small_virtorbs=len([i for i in natocc if i < self.FNO_thresh])
+                    print("Num. virtual orbitals below threshold:", num_small_virtorbs)
+                    #List of frozen orbitals
+                    virt_frozen= [num_scf_orbitals_alpha-i for i in range(1,num_small_virtorbs+1)][::-1]
+                    print("List of frozen virtuals:", virt_frozen)            
+                    print("List of frozen virtuals:", virt_frozen)            
+                    self.frozen_orbital_indices = self.frozen_orbital_indices + virt_frozen
+        
+            #CCSD-part as RCCSD or UCCSD
+            print()
+            print("All frozen_orbital_indices:", self.frozen_orbital_indices)
+            print("Total number of frozen orbitals:", len(self.frozen_orbital_indices))
+            print("Total number of orbitals:", num_scf_orbitals_alpha)
+            print("Number of active orbitals:", num_scf_orbitals_alpha-len(self.frozen_orbital_indices))
+            print()
+            print("Now starting CCSD calculation")
+            if self.scf_type == "RHF":
+                cc = self.pyscf_cc.CCSD(mf, self.frozen_orbital_indices,mo_coeff=mo_coefficients)
+            elif self.scf_type == "UHF":
+                print("here")
+                cc = self.pyscf_cc.UCCSD(mf,self.frozen_orbital_indices,mo_coeff=mo_coefficients)
+                
+            elif self.scf_type == "RKS":
+                print("Warning: CCSD on top of RKS determinant")
+                cc = self.pyscf_cc.CCSD(mf.to_rhf(), self.frozen_orbital_indices,mo_coeff=mo_coefficients)
+            elif self.scf_type == "UKS":
+                print("Warning: CCSD on top of UKS determinant")
+                cc = self.pyscf_cc.UCCSD(mf.to_uhf(),self.frozen_orbital_indices,mo_coeff=mo_coefficients)
+            
+            #Switch to integral-direct CC if user-requested
+            #NOTE: Faster but only possible for small/medium systems
+            cc.direct = self.CC_direct
+            
+            result = cc.run()
+            print("Reference energy:", result.e_hf)
+            #(T) part
+            if self.CCmethod == 'CCSD(T)':
+                print("Calculating triples ")
+                et = cc.ccsd_t()
+                print("Triples energy:", et)
+                self.energy = result.e_tot + et
+                print("Final CCSD(T) energy:", self.energy)
+        #SCF RUN
+        else:
+            scf_result = mf.run()
+            #Get SCFenergy as energy
+            self.energy = scf_result.e_tot
+            print("SCF energy components:", scf_result.scf_summary)
+        
         #Grab energy and gradient
+        #NOTE: only SCF supported for now
         if Grad==True:
             if PC is True:
                 print("THIS IS NOT CONFIRMED TO WORK!!!!!!!!!!!!")
@@ -187,6 +389,7 @@ class PySCFTheory:
 
 
         #TODO: write in error handling here
+        print()
         print(BC.OKBLUE, BC.BOLD, "------------ENDING PYSCF INTERFACE-------------", BC.END)
         if Grad == True:
             print("Single-point PySCF energy:", self.energy)
