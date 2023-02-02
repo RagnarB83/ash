d2a488abf5c014b8bb44769109ab3de39139b647
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-02-02 17:35:09 +0100

    final scf fix

--- interfaces/interface_pyscf.py
+++ interfaces/interface_pyscf.py
@@ -363,6 +363,7 @@ class PySCFTheory:
         #####################
         if self.SCF is True:
             print("Running SCF")
+            self.mf.verbose=self.verbose_setting
             if self.write_chkfile_name != None:
                 self.mf.chkfile = self.write_chkfile_name
             else:
