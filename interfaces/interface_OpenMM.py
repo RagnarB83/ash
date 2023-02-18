26a62e8b25572e422462d0baffae1d612d6f5d25
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-02-18 23:51:20 +0100

    - moved mdtraj functions to separate interface file - started PES rewrite - Gentle_MD_warm_up now images trajectories for convenience - New option, called by Gentle_MD_warm_up calculates RMSF values for trajectory, helps identify problematic atoms

--- interfaces/interface_OpenMM.py
+++ interfaces/interface_OpenMM.py
@@ -21,6 +21,7 @@ from ash.interfaces.interface_xtb import xTBTheory, grabatomcharges_xTB
 from ash.interfaces.interface_ORCA import ORCATheory, grabatomcharges_ORCA, chargemodel_select
 from ash.modules.module_singlepoint import Singlepoint
 from ash.interfaces.interface_plumed import MTD_analyze
+from ash.interfaces.interface_mdtraj import MDtraj_import, MDtraj_imagetraj, MDtraj_RMSF
 
 
 class OpenMMTheory:
@@ -2319,165 +2320,6 @@ def OpenMM_Modeller(pdbfile=None, forcefield=None, xmlfile=None, waterxmlfile=No
     return openmmobject, fragment
 
 
-def MDtraj_import_():
-    print("Importing mdtraj (https://www.mdtraj.org)")
-    try:
-        import mdtraj
-    except ImportError:
-        print("Problem importing mdtraj. Try: 'pip install mdtraj' or 'conda install -c conda-forge mdtraj'")
-        ashexit()
-    return mdtraj
-
-
-# anchor_molecules. Use if automatic guess fails
-def MDtraj_imagetraj(trajectory, pdbtopology, format='DCD', unitcell_lengths=None, unitcell_angles=None,
-                     solute_anchor=None):
-    #Trajectory basename
-    traj_basename = os.path.splitext(trajectory)[0]
-    #PDB-file basename
-    pdb_basename = os.path.splitext(pdbtopology)[0]
-    
-    #Import mdtraj library
-    mdtraj = MDtraj_import_()
-
-    # Load trajectory
-    print("Loading trajectory using mdtraj.")
-    traj = mdtraj.load(trajectory, top=pdbtopology)
-
-    #Also load the pdbfile as a trajectory-snapshot (in addition to being topology)
-    pdbsnap = mdtraj.load(pdbtopology, top=pdbtopology)
-    pdbsnap_imaged = pdbsnap.image_molecules()
-
-    numframes = len(traj._time)
-    print("Found {} frames in trajectory.".format(numframes))
-    print("PBC information in trajectory:")
-    print("Unitcell lengths:", traj.unitcell_lengths[0])
-    print("Unitcell angles", traj.unitcell_angles[0])
-    # If PBC information is missing from traj file (OpenMM: Charmmfiles, Amberfiles option etc) then provide this info
-    if unitcell_lengths is not None:
-        print("unitcell_lengths info provided by user.")
-        unitcell_lengths_nm = [i / 10 for i in unitcell_lengths]
-        traj.unitcell_lengths = np.array(unitcell_lengths_nm * numframes).reshape(numframes, 3)
-        traj.unitcell_angles = np.array(unitcell_angles * numframes).reshape(numframes, 3)
-    # else:
-    #    print("Missing PBC info. This can be provided by unitcell_lengths and unitcell_angles keywords")
-
-    # Manual anchor if needed
-    # NOTE: not sure how well this works but it's something
-    if solute_anchor is True:
-        anchors = [set(traj.topology.residue(0).atoms)]
-        print("anchors:", anchors)
-        # Re-imaging trajectory
-        imaged = traj.image_molecules(anchor_molecules=anchors)
-    else:
-        imaged = traj.image_molecules()
-
-    # Save trajectory in format
-    if format == 'DCD':
-        imaged.save(traj_basename + '_imaged.dcd')
-        print("Saved reimaged trajectory:", traj_basename + '_imaged.dcd')
-    elif format == 'PDB':
-        imaged.save(traj_basename + '_imaged.pdb')
-        print("Saved reimaged trajectory:", traj_basename + '_imaged.pdb')
-    else:
-        print("Unknown trajectory format.")
-
-    #Save PDB-snapshot
-    pdbsnap_imaged.save(pdb_basename + '_imaged.pdb')
-    print("Saved reimaged PDB-file:", pdb_basename + '_imaged.pdb')
-    #Return last frame as coords or ASH fragment ?
-    #Last frame coordinates as Angstrom
-    lastframe=imaged[-1]._xyz[-1]*10
-
-    return lastframe
-
-
-# Slicing trajectory. Mostly to grab specific snapshot
-#TODO: allow option to grab by ps? Requires information about timestep and traj-frequency
-def MDtraj_slice(trajectory, pdbtopology, format='PDB', frames=None):
-
-    if frames is None:
-        print("frames needs to be set")
-        ashexit()
-
-    #Trajectory basename
-    traj_basename = os.path.splitext(trajectory)[0]
-    
-    #Import mdtraj library
-    mdtraj = MDtraj_import_()
-
-    # Load trajectory
-    print("Loading trajectory using mdtraj.")
-    traj = mdtraj.load(trajectory, top=pdbtopology)
-    print(f"This trajectory contains {traj.n_frames} frames")
-    #Slicing trajectory
-    print("Slicing trajectory using frame selection:", frames)
-    tslice = traj[frames[0]:frames[1]]
-    print(f"Trajectory slice contains {tslice.n_frames} frames")
-    if tslice.n_frames == 0:
-        print(f"0 frames found when slicing. You probably should do: frames=[{frames[0]},{frames[1]+1}] instead")
-        print("Exiting")
-        ashexit()
-
-    # Save trajectory in format
-    if format == 'DCD':
-        tslice.save(traj_basename + f'_frame{frames[0]}_{frames[1]}.dcd')
-        print("Saved sliced trajectory:", traj_basename + f'_frame{frames[0]}_{frames[1]}.dcd')
-    elif format == 'PDB':
-        tslice.save(traj_basename + f'_frame{frames[0]}_{frames[1]}.pdb')
-        print("Saved sliced trajectory:", traj_basename + f'_frame{frames[0]}_{frames[1]}.pdb')
-    else:
-        print("Unknown trajectory format.")
-    return
-
-def MDAnalysis_transform(topfile, trajfile, solute_indices=None, trajoutputformat='PDB', trajname="MDAnalysis_traj"):
-    # Load traj
-    print("MDAnalysis interface: transform")
-
-    try:
-        import MDAnalysis as mda
-        import MDAnalysis.transformations as trans
-    except ImportError:
-        print("Problem importing MDAnalysis library.")
-        print("Install via: 'pip install mdtraj'")
-        ashexit()
-
-    print("Loading trajecory using MDAnalysis")
-    print("Topology file:", topfile)
-    print("Trajectory file:", trajfile)
-    print("Solute_indices:", solute_indices)
-    print("Trajectory output format:", trajoutputformat)
-    print("Will unwrap solute and center in box.")
-    print("Will then wrap full system.")
-
-    # Load trajectory
-    u = mda.Universe(topfile, trajfile, in_memory=True)
-    print(u.trajectory.ts, u.trajectory.time)
-
-    # Grab solute
-    numatoms = len(u.atoms)
-    solutenum = len(solute_indices)
-    solute = u.atoms[:solutenum]
-    # solvent = u.atoms[solutenum:numatoms]
-    fullsystem = u.atoms[:numatoms]
-    elems_list = list(fullsystem.types)
-    # Guess bonds. Could also read in vdW radii. Could also read in connectivity from ASH if this fails
-    solute.guess_bonds()
-    # Unwrap solute, center solute and wraps full system (or solvent)
-    workflow = (trans.unwrap(solute),
-                trans.center_in_box(solute, center='mass'),
-                trans.wrap(fullsystem, compound='residues'))
-
-    u.trajectory.add_transformations(*workflow)
-    if trajoutputformat == 'PDB':
-        fullsystem.write(trajname + ".pdb", frames='all')
-
-    # TODO: Distinguish between transforming whole trajectory vs. single geometry
-    # Maybe just read in single-frame trajectory so that things are general
-    # Returning last frame. To be used in ASH workflow
-    lastframe = u.trajectory[-1]
-
-    return elems_list, lastframe._pos
 
 
 # Assumes all atoms present (including hydrogens)
@@ -3906,7 +3748,7 @@ CV1_indices={CV1_atoms}, CV2_indices={CV2_atoms}, plumed_energy_unit='kj/mol', P
 
 #
 def Gentle_warm_up_MD(theory=None, fragment=None, time_steps=[0.0005,0.001,0.004], steps=[10,50,10000], 
-    temperatures=[1,10,300], check_gradient_first=False, gradient_threshold=100):
+    temperatures=[1,10,300], check_gradient_first=True, gradient_threshold=100, use_mdtraj=True):
     print_line_with_mainheader("Gentle_warm_up_MD")
     if theory is None or fragment is None:
         print("Gentle_warm_up_MD requires theory (OpenMM object) and fragment")
@@ -3924,22 +3766,34 @@ def Gentle_warm_up_MD(theory=None, fragment=None, time_steps=[0.0005,0.001,0.004
         SP_result = Singlepoint(theory=theory, fragment=fragment, Grad=True)
         badindices = check_gradient_for_bad_atoms(fragment=fragment,gradient=SP_result.gradient, threshold=gradient_threshold)
         if len(badindices) > 0:
-            print(f"Some bad gradients on atoms were found {len(badindices)}")
-            print("This may be an indication of a badly set up system or that constraints are needed for these atoms")
-            print("Gentle_warm_up_MD may not be able to warm up the system correctly but will try")
+            print(f"\nNumber of atoms with large forces: {len(badindices)}")
+            print("Suggests a bad system geometry or that atoms need constraints (might be present already)")
+            print("Gentle_warm_up_MD will go on")
 
-    print(f"{len(steps)} MD-runs have been defined")
+    print(f"\n{len(steps)} MD-runs have been defined")
     for num, (ts, step, temp) in enumerate(zip(time_steps, steps, temperatures)):
-        print(f"MD-step {num} Number of simulation steps: {step} with timestep:{ts} and temperature: {temp} K")
+        print(f"MD-step {num} Number of simulation steps: {step} with timestep: {ts} and temperature: {temp} K")
 
     print();print()
     #Gentle heating up protocol
     for num, (ts, step, temp) in enumerate(zip(time_steps, steps, temperatures)):
-        print(f"Now running MD-run {num}. Number of steps: {step} with timestep:{ts} and temperature: {temp} K")
+        print(f"\nNow running MD-run {num}. Number of steps: {step} with timestep:{ts} and temperature: {temp} K")
         print("Will write trajectory to file:", 'NVTtrajectorystep_'+str(num)+'.dcd')
         OpenMM_MD(fragment=fragment, theory=theory, timestep=ts, simulation_steps=step, traj_frequency=1, temperature=temp,
             integrator='LangevinMiddleIntegrator', coupling_frequency=1, trajfilename='NVTtrajectorystep_'+str(num), trajectory_file_option='DCD')
-    
+        
+        #Running mdtraj after each sim
+        if use_mdtraj is True:
+            print("Trying to load mdtraj for basic analysis of trajectory")
+            try:
+                print("Imaging trajectory")
+                MDtraj_imagetraj('NVTtrajectorystep_'+str(num)+'.dcd', "final_MDfrag_laststep.pdb")
+                print("Running RMS Fluctuation analysis on trajectory")
+                MDtraj_RMSF('NVTtrajectorystep_'+str(num)+"_imaged.dcd", "final_MDfrag_laststep.pdb", print_largest_values=True, 
+                    threshold=0.005, largest_values=10)
+            except ImportError:
+                print("mdtraj library could not be imported. Skipping")
+
     print("Gentle_warm_up_MD finished successfully!")
 
     return
