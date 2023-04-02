7ebde8fd590a64ac1dbb1e8e8a328b4a2136688a
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-04-02 16:24:58 +0200

    NWChem interface: PC embedding

--- interfaces/interface_NWChem.py
+++ interfaces/interface_NWChem.py
@@ -10,8 +10,7 @@ from ash.functions.functions_parallel import check_OpenMPI
 
 #Reasonably flexible NWChem interface: NWChem input should be specified by nwcheminput multi-line string.
 #Everything except geometry
-#Works for RHF,UHF,RKS,UKS, MP2, CCSD(T) w/wo TCE. 
-#TODO: PC embedding
+#Works for RHF,UHF,RKS,UKS, MP2, CCSD(T) w/wo TCE. PC available: energy and gradient
 #TODO: Constrained DFT
 #TODO: GPU support for CC
 #TODO: ADFT
@@ -121,53 +120,46 @@ class NWChemTheory:
                 qm_elems = elems
 
         #Write PC to disk
-        #if PC is True:
-        #    create_NWChem_pcfile_general(current_MM_coords,MMcharges, filename=self.filename)
-
-        #Grab energy and gradient
-        if Grad==True:
-            #if PC is True:
-            #    write_NWChem_input(self.nwcheminput,charge,mult,qm_elems,current_coords, method=self.method,
-            #        Grad=True, PCfile=self.filename+'.pc', filename=self.filename, openshell=self.openshell,
-            #        tce=self.tce)
-            #else:
-            write_NWChem_input(self.nwcheminput,charge,mult,qm_elems,current_coords, method=self.method,
-                Grad=True, filename=self.filename, openshell=self.openshell, tce=self.tce)
-            
-            #Run NWChem
-            run_NWChem(self.nwchemdir,self.filename,numcores=self.numcores)
-
-            self.energy=grab_energy_nwchem(self.filename+'.out',method=self.method, tce=self.tce)
-            if PC is True:
-                self.gradient,self.pcgradient = grab_gradient_NWChem(self.filename+'.out',len(current_coords), numpc=len(MMcharges))
-            else:
-                self.gradient,self.pcgradient = grab_gradient_NWChem(self.filename+'.out',len(current_coords))
+        if PC is True:
+            print("pc true")
+            create_NWChem_pcfile_general(current_MM_coords,MMcharges, filename=self.filename)
+            pcfile=self.filename+'.pc'
         else:
-            if PC is True:
-                write_NWChem_input(self.nwcheminput,charge,mult,qm_elems,current_coords,Grad=False, 
-                    PCfile=self.filename+'.pc', method=self.method, openshell=self.openshell, tce=self.tce)
-            else:
-                write_NWChem_input(self.nwcheminput,charge,mult,qm_elems,current_coords,Grad=False, 
-                method=self.method, openshell=self.openshell, tce=self.tce)
-            
-            #Run NWChem
-            run_NWChem(self.nwchemdir,self.filename,numcores=self.numcores)
-            
-            self.energy=grab_energy_nwchem(self.filename+'.out',method=self.method, tce=self.tce)
+            pcfile=None
+        
+        #Write inputfile
+        write_NWChem_input(self.nwcheminput,charge,mult,qm_elems,current_coords, method=self.method,
+                Grad=Grad, PCfile=pcfile, filename=self.filename, openshell=self.openshell, tce=self.tce)
 
-        #TODO: write in error handling here
+        #Run NWChem
+        run_NWChem(self.nwchemdir,self.filename,numcores=self.numcores)
+
+        #Grab energy
+        self.energy=grab_energy_nwchem(self.filename+'.out',method=self.method, tce=self.tce)
+        print(f"Single-point {self.theorynamelabel} energy:", self.energy)
         print(BC.OKBLUE, BC.BOLD, f"------------ENDING {self.theorynamelabel} INTERFACE-------------", BC.END)
-        if Grad == True:
-            print(f"Single-point {self.theorynamelabel} energy:", self.energy)
-            print_time_rel(module_init_time, modulename=f'{self.theorynamelabel} run', moduleindex=2)
+        
+        #Grab gradient if calculated
+        if Grad is True:
+            #Grab gradient
+            self.gradient = grab_gradient_NWChem(self.filename+'.out',len(current_coords))
+            #Grab PCgradient from separate file
             if PC is True:
+                self.pcgradient = grab_pcgradient_NWChem(f'{self.filename}.bqforce.dat',len(MMcharges))
+                print_time_rel(module_init_time, modulename=f'{self.theorynamelabel} run', moduleindex=2)
                 return self.energy, self.gradient, self.pcgradient
             else:
+                print_time_rel(module_init_time, modulename=f'{self.theorynamelabel} run', moduleindex=2)
                 return self.energy, self.gradient
+        #Returning energy without gradient
         else:
-            print(f"Single-point {self.theorynamelabel} energy:", self.energy)
             print_time_rel(module_init_time, modulename=f'{self.theorynamelabel} run', moduleindex=2)
             return self.energy
+
+
+################################
+# Independent NWChem functions
+################################
 
 def run_NWChem(nwchemdir,filename,numcores=1):
     with open(filename+'.out', 'w') as ofile:
@@ -178,21 +170,30 @@ def run_NWChem(nwchemdir,filename,numcores=1):
 
 def write_NWChem_input(nwcheminput,charge,mult,elems,coords, filename='nwchem',
     PCfile=None, Grad=True, method='scf', openshell=False, tce=False):
-    pckeyword="no"
     if Grad is True:
         jobdirective='gradient'
     else:
         jobdirective='energy'
-    if PCfile is not None:
-        pckeyword=PCfile
     with open(f"{filename}.nw", 'w') as inpfile:
         inpfile.write('#NWChem input\n')
+        inpfile.write(f'echo\n')
         inpfile.write(f'start {filename}\n')
         inpfile.write(f'charge {charge}\n')
-        inpfile.write(f'geometry units angstrom\n')
+        #Turning off symmetry by default (important for ASH opt and QM/MM).
+        #TODO: Re-enable symmetry if wanted by user? Useful for using symmetry in expensive CC calculations
+        inpfile.write(f'geometry nocenter noautosym units angstrom noautoz print\n')
         for el,c in zip(elems,coords):
             inpfile.write(f'{el } {c[0]} {c[1]} {c[2]}\n')
         inpfile.write(f'end\n')
+        #Pointcharge embedding
+        if PCfile != None:
+            inpfile.write(f'bq\n')
+            #Loading PC file. Telling NWChem about format
+            inpfile.write(f'load {PCfile} format 2 3 4 1\n')
+            #PC forces
+            inpfile.write(f'force\n')
+            inpfile.write(f'end\n')
+
         if openshell is True:
             #HF or post-HF case
             if method == 'scf' or method =='mp2' or 'ccsd' in method or 'tce' in method:
@@ -200,6 +201,7 @@ def write_NWChem_input(nwcheminput,charge,mult,elems,coords, filename='nwchem',
                 inpfile.write(f'uhf doublet\n')
                 inpfile.write(f'nopen {mult-1}\n')
                 inpfile.write(f'end\n')
+            #DFT
             elif method == 'dft':
                 inpfile.write(f'dft\n')
                 inpfile.write(f'odft\n')
@@ -242,53 +244,49 @@ def grab_energy_nwchem(outfile,method=None,tce=False):
                 energy=float(line.split()[-1])
     return energy
 
-def grab_gradient_NWChem(outfile,numatoms,numpc=None):
+def grab_gradient_NWChem(outfile,numatoms):
     grad_grab=False
-    #pcgrad_grab=False
-
     gradient=np.zeros((numatoms,3))
-    pc_gradient=None
-    #if numpc is not None:
-    #    pc_gradient=np.zeros((numpc,3))
     atomcount=0
-    #pccount=0
     with open(outfile) as o:
         for i,line in enumerate(o):
             if grad_grab is True:
                 if 'x' in line:
                     continue
-                #if len(line.split()) == 3:
                 gradient[atomcount,0] = float(line.split()[-3])
                 gradient[atomcount,1] = float(line.split()[-2])
                 gradient[atomcount,2] = float(line.split()[-1])
                 atomcount+=1
-                ##else:
-                #    continue
-            #if pcgrad_grab is True:
-            #    if len(line.split()) == 3:
-            #        pc_gradient[pccount,0] = float(line.split()[0])
-            #        pc_gradient[pccount,1] = float(line.split()[1])
-            #        pc_gradient[pccount,2] = float(line.split()[2])
-            #        pccount+=1
-            #    if pccount == numpc:
-            #        pcgrad_grab=False
             if 'Net gradient' in line:
                 grad_grab=False
-                pcgrad_grab=False
             if 'atom               coordinates                        gradient' in line:
                 grad_grab=True
-            #if '------- MM / Point charge part' in line:
-            #    pcgrad_grab=True
             if atomcount == numatoms:
                  grad_grab=False
-    return gradient, pc_gradient
+    return gradient
 
 
-# pc-coords in Å
-# def create_NWChem_pcfile_general(coords,pchargelist,filename='nwchem'):
-#     with open(filename+'.pc', 'w') as pcfile:
-#         pcfile.write(str(len(pchargelist))+'\n')
-#         pcfile.write('\n')
-#         for p,c in zip(pchargelist,coords):
-#             line = "{} {} {} {}".format(p, c[0], c[1], c[2])
-#             pcfile.write(line+'\n')
+#Grab PC gradient from NWchem written file
+def grab_pcgradient_NWChem(pcgradfile,numpc):
+    pc_gradient=np.zeros((numpc,3))
+    pccount=0
+    with open(pcgradfile) as o:
+        for i,line in enumerate(o):
+            if '#' not in line:
+                pc_gradient[pccount,0] = float(line.split()[0])
+                pc_gradient[pccount,1] = float(line.split()[1])
+                pc_gradient[pccount,2] = float(line.split()[2])
+                pccount+=1 
+    if pccount != numpc:
+        print("Problem grabbing PC gradient from file:", pcgradfile)
+        ashexit()
+    return pc_gradient
+
+# Write PC-coords and charges in Å
+def create_NWChem_pcfile_general(coords,pchargelist,filename='nwchem'):
+    with open(filename+'.pc', 'w') as pcfile:
+        pcfile.write(str(len(pchargelist))+'\n')
+        pcfile.write('\n')
+        for p,c in zip(pchargelist,coords):
+            line = "{} {} {} {}".format(p, c[0], c[1], c[2])
+            pcfile.write(line+'\n')
