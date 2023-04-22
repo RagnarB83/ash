f641378137962a6c76bca450931715ba2fb023ee
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-04-22 12:50:50 +0200

    free energy functions globablly available

--- __init__.py
+++ __init__.py
@@ -100,7 +100,7 @@ from ash.interfaces.interface_PyMBE import PyMBETheory
 # MM: external and internal
 from ash.interfaces.interface_OpenMM import OpenMMTheory, OpenMM_MD, OpenMM_MDclass, OpenMM_Opt, OpenMM_Modeller, \
      OpenMM_box_relaxation, write_nonbonded_FF_for_ligand, \
-        OpenMM_metadynamics, Gentle_warm_up_MD, check_gradient_for_bad_atoms
+        OpenMM_metadynamics, Gentle_warm_up_MD, check_gradient_for_bad_atoms, get_free_energy_from_biasfiles, free_energy_from_bias_array
 from ash.modules.module_MM import NonBondedTheory, UFFdict, UFF_modH_dict, LJCoulpy, coulombcharge, LennardJones, \
     LJCoulombv2, LJCoulomb, MMforcefield_read
 #MDtraj
