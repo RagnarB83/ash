6c7f3b00216e83a3d75a369047a4fa7cfd4bb1e2
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-04-23 16:17:32 +0200

    fix

--- interfaces/interface_OpenMM.py
+++ interfaces/interface_OpenMM.py
@@ -3968,33 +3968,15 @@ PRINT STRIDE={strideval} ARG=CV1,metad.bias FILE=COLVAR
         ashexit()
     #MULTIPLE WALKERS
     if multiplewalkers is True:
-        walker_string=f"""WALKERS_N={walkernum}
-WALKERS_ID=WALKERID
-WALKERS_DIR={biasdir}
-WALKERS_RSTRIDE={strideval}
+        walker_string=f"""WALKERS_N={walkernum} WALKERS_ID=WALKERID WALKERS_DIR={biasdir} WALKERS_RSTRIDE={strideval}
         """
-        
-        plumedinput = f"""
-        {cv1atom_line}
+        plumedinput = f"""{cv1atom_line}
         {cv2atom_line}
-        METAD ...
-            ARG=CV1,CV2 SIGMA={sigma_cv1},{sigma_cv2} GRID_MIN={grid_min1},{grid_min2} GRID_MAX={grid_max1},{grid_max2} HEIGHT={height} PACE={paceval} TEMP={temperature} BIASFACTOR={biasfactor} FMT=%14.6f
-        {walker_string}
-        ...
+        metad: METAD ARG=CV1,CV2 SIGMA={sigma_cv1},{sigma_cv2} GRID_MIN={grid_min1},{grid_min2} GRID_MAX={grid_max1},{grid_max2} HEIGHT={height} PACE={paceval} TEMP={temperature} BIASFACTOR={biasfactor} FMT=%14.6f {walker_string}
         PRINT STRIDE={strideval} ARG=CV1,CV2,metad.bias FILE=COLVAR
         """
     else:
         walker_string=""
-        #
-        #plumedinput = f"""
-        #{cv1atom_line}
-        #{cv2atom_line}
-        #METAD ...
-        #    ARG=CV1,CV2 SIGMA={sigma_cv1},{sigma_cv2} GRID_MIN={grid_min1},{grid_min2} GRID_MAX={grid_max1},{grid_max2} HEIGHT={height} PACE={paceval} TEMP={temperature} BIASFACTOR={biasfactor} FMT=%14.6f
-        #{walker_string}
-        #...
-        #PRINT STRIDE={strideval} ARG=CV1,CV2,metad.bias FILE=COLVAR
-        #"""
         plumedinput = f"""{cv1atom_line}
         {cv2atom_line}
         metad: METAD ARG=CV1,CV2 SIGMA={sigma_cv1},{sigma_cv2} GRID_MIN={grid_min1},{grid_min2} GRID_MAX={grid_max1},{grid_max2} HEIGHT={height} PACE={paceval} TEMP={temperature} BIASFACTOR={biasfactor} FMT=%14.6f {walker_string}
