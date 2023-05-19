b2986c6fe7e2e950154f216d167fb8d8a9195c60
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-05-19 10:23:43 +0200

    solvate_small_molecule global

--- __init__.py
+++ __init__.py
@@ -100,7 +100,7 @@ from ash.interfaces.interface_PyMBE import PyMBETheory
 
 # MM: external and internal
 from ash.interfaces.interface_OpenMM import OpenMMTheory, OpenMM_MD, OpenMM_MDclass, OpenMM_Opt, OpenMM_Modeller, \
-     OpenMM_box_relaxation, write_nonbonded_FF_for_ligand, \
+     OpenMM_box_relaxation, write_nonbonded_FF_for_ligand, solvate_small_molecule \
         OpenMM_metadynamics, Gentle_warm_up_MD, check_gradient_for_bad_atoms, get_free_energy_from_biasfiles, \
         free_energy_from_bias_array,metadynamics_plot_data
 from ash.modules.module_MM import NonBondedTheory, UFFdict, UFF_modH_dict, LJCoulpy, coulombcharge, LennardJones, \
