2b3afd241661cbdea17de502e4e81e34e296022b
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-08-04 12:45:06 +0200

    fix

--- interfaces/interface_knarr.py
+++ interfaces/interface_knarr.py
@@ -74,7 +74,7 @@ optimizer = {"OPTIM_METHOD": "LBFGS", "MAX_ITER": 200, "TOL_MAX_FORCE": 0.01,
 #Threshold settings for CI-NEB part are the same as in the NEB-TS of ORCA
 def NEBTS(reactant=None, product=None, theory=None, images=8, CI=True, free_end=False, maxiter=100, IDPPonly=False,
         conv_type="ALL", tol_scale=10, tol_max_fci=0.10, tol_rms_fci=0.05, tol_max_f=1.03, tol_rms_f=0.51,
-        tol_turn_on_ci=1.0,  runmode='serial', numcores=1, charge=None, mult=None, printlevel=1, ActiveRegion=False, actatoms=None,
+        tol_turn_on_ci=1.0,  runmode='serial', runmode_numfreq='serial', numcores=1, charge=None, mult=None, printlevel=1, ActiveRegion=False, actatoms=None,
         interpolation="IDPP", idpp_maxiter=700, idpp_springconst=5.0, restart_file=None, TS_guess_file=None, mofilesdir=None, 
         OptTS_maxiter=100, OptTS_print_atoms_list=None, OptTS_convergence_setting=None, OptTS_conv_criteria=None, OptTS_coordsystem='tric',
         hessian_for_TS=None, modelhessian='unit', tsmode_tangent_threshold=0.1, subfrctor=1):
@@ -128,13 +128,14 @@ def NEBTS(reactant=None, product=None, theory=None, images=8, CI=True, free_end=
     ##############################
     #Hessianfile should be a simple text file with 1 row per line, values space-separated and no header.
     #Default: 
+    print("WARNING: runmode for Numfreq currently defaults to serial (problems with parallel version). Will be fixed in future.")
     if hessian_for_TS == None:
         print("Using default hessian_for_TS option")
         #If dualtheory we just do a Numfreq in regular mode
         if isinstance(theory,ash.DualTheory):
             print("Dualtheory active. Doing Numfreq using regular mode.")
             #NOTE: Regular will probably involve a theory 2 correction. We could switch to theory1 solely instead here
-            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, runmode=runmode, numcores=numcores)
+            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, runmode=runmode_numfreq, numcores=numcores)
             hessianfile="Hessian_from_dualtheory"
             shutil.copyfile("Numfreq_dir/Hessian",hessianfile)
             hessianoption='file:'+str(hessianfile)
@@ -142,7 +143,7 @@ def NEBTS(reactant=None, product=None, theory=None, images=8, CI=True, free_end=
             #NOTE: More sensible default than below
             #NOTE: This is different from partial approach below. Need to change
             print("QM/MM Theory is recognized. Doing by default partial Hessian using whole QM-region")
-            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=1, runmode=runmode, 
+            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=1, runmode=runmode_numfreq, 
                                     numcores=numcores, hessatoms=theory.qmatoms)
 
             #Combine partial exact Hessian with model Hessian(Almloef, Lindh, Schlegel or unit)
@@ -155,7 +156,7 @@ def NEBTS(reactant=None, product=None, theory=None, images=8, CI=True, free_end=
             hessianoption='file:'+str(hessianfile)            
         else:
             print("Doing Numfreq")
-            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=1, runmode=runmode, numcores=numcores)
+            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=1, runmode=runmode_numfreq, numcores=numcores)
             hessianfile="Hessian_from_theory"
             shutil.copyfile("Numfreq_dir/Hessian",hessianfile)
             hessianoption='file:'+str(hessianfile)
@@ -164,13 +165,13 @@ def NEBTS(reactant=None, product=None, theory=None, images=8, CI=True, free_end=
     # Tell geomeTRIC to calculate exact Hessian in the beginning
     elif hessian_for_TS == '2point':
             print("Doing Numfreq 2-point approximation")
-            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=2, runmode=runmode, numcores=numcores)
+            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=2, runmode=runmode_numfreq, numcores=numcores)
             hessianfile="Hessian_from_theory"
             shutil.copyfile("Numfreq_dir/Hessian",hessianfile)
             hessianoption='file:'+str(hessianfile)
     elif hessian_for_TS == '1point':
             print("Doing Numfreq 1-point approximation")
-            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=1, runmode=runmode, numcores=numcores)
+            result_freq = ash.NumFreq(theory=theory, fragment=SP, printlevel=0, npoint=1, runmode=runmode_numfreq, numcores=numcores)
             hessianfile="Hessian_from_theory"
             shutil.copyfile("Numfreq_dir/Hessian",hessianfile)
             hessianoption='file:'+str(hessianfile)
