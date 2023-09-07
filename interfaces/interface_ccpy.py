a9f9f244ba5d050b058a831027501cb895614d4a
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-09-07 11:58:38 +0200

    ccpy : more methods

--- interfaces/interface_ccpy.py
+++ interfaces/interface_ccpy.py
@@ -168,7 +168,19 @@ class ccpyTheory:
             print("Non-adaptive CC calculation.")
             #driver.options["maximum_iterations"] = 1000 # 4 Sigma state requires ~661 iterations in left-CCSD
             #driver.options["davidson_max_subspace_size"] = 50
-            driver.run_cc(method=self.method)
+            if self.method.lower() == "ccsd" or self.method.lower() == "ccsdt1" or self.method.lower() == "ccsdt":
+                driver.run_cc(method=self.method)
+            elif self.method.lower() == "ccsd(t)" or self.method == "cct3":
+                driver.run_cc(method="ccsd")
+                driver.run_ccp3(method="ccsd(t)")
+            elif self.method.lower() == "crcc23":
+                driver.run_cc(method="ccsd")
+                driver.run_hbar(method="ccsd")
+                driver.run_leftcc(method="left_ccsd")
+                driver.run_ccp3(method="crcc23")
+
+            elif 'eom' in self.method.lower():
+                driver.run_eomcc(method=self.method.lower(), state_index=1)
             #driver.run_hbar(method="ccsd")
             #driver.run_guess(method="cis", multiplicity=1, nroot=10)
             #driver.run_eomcc(method="eomccsd", state_index=selected_states[1:])
