3344bb517b6fd2280397651a7b7b975e470fec51
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-10-22 19:23:02 +0200

    fix

--- interfaces/interface_NWChem.py
+++ interfaces/interface_NWChem.py
@@ -17,7 +17,7 @@ from ash.functions.functions_parallel import check_OpenMPI
 
 #NWChem Theory object.
 class NWChemTheory:
-    def __init__(self, nwchemdir=None, filename='nwchem', openshell=False, printlevel=2, label="NWChem"
+    def __init__(self, nwchemdir=None, filename='nwchem', openshell=False, printlevel=2, label="NWChem",
                 nwcheminput=None, method='scf', tce=False, numcores=1):
 
         self.theorynamelabel="NWChem"
