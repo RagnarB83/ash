345bf5090dbc15958c55cdab14de9f9b2b60d6c2
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-10-24 14:29:21 +0200

    fix

--- interfaces/interface_pyscf.py
+++ interfaces/interface_pyscf.py
@@ -935,12 +935,12 @@ class PySCFTheory:
                 D1_diagnostic_beta = pyscf.cc.ccsd.get_d1_diagnostic(cc.t1[1])
                 print("D1 diagnostic (alpha):", D1_diagnostic_alpha)
                 print("D1 diagnostic (beta):", D1_diagnostic_beta)
+                D2_diagnostic_alpha = pyscf.cc.ccsd.get_d2_diagnostic(cc.t2[0])
+                D2_diagnostic_beta = pyscf.cc.ccsd.get_d2_diagnostic(cc.t2[1])
+                print("D2 diagnostic (alpha):", D2_diagnostic_alpha)
+                print("D2 diagnostic (beta):", D2_diagnostic_beta)
             except IndexError:
-                print("Problem, calculating D1 diagnostic. Skipping")
-            D2_diagnostic_alpha = pyscf.cc.ccsd.get_d2_diagnostic(cc.t2[0])
-            D2_diagnostic_beta = pyscf.cc.ccsd.get_d2_diagnostic(cc.t2[1])
-            print("D2 diagnostic (alpha):", D2_diagnostic_alpha)
-            print("D2 diagnostic (beta):", D2_diagnostic_beta)
+                print("Problem, calculating D1/D2 diagnostics. Skipping")
         else:
             T1_diagnostic = pyscf.cc.ccsd.get_t1_diagnostic(cc.t1)
             print("T1 diagnostic:", T1_diagnostic)
