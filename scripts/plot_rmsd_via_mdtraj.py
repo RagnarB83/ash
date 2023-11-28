33f4fe5613fe14d029be8cac7b59c49862b16c84
RagnarB83 <ragnar.bjornsson@gmail.com>
2023-11-28 13:34:00 +0100

    fix

--- scripts/plot_rmsd_via_mdtraj.py
+++ scripts/plot_rmsd_via_mdtraj.py
@@ -2,11 +2,12 @@ from ash import *
 import mdtraj
 
 #Plotting RMSD of trajectory using mdtraj
+#Usage: python3 plot_rmsd_via_mdtraj.py trajfilename pdbfilename
 
-#The name of the PDB-file to use for topology
-pdbfile="frag-minimized.pdb"
 #The name of the trajectory file to load
-trajfile="relaxbox_NPT.dcd"
+trajfile=sys.argv[1]
+#The name of the PDB-file to use for topology
+pdbfile=sys.argv[2]
 
 #Loading using mdtraj
 system = mdtraj.load(pdbfile)
@@ -22,7 +23,7 @@ rmsd_heavy = mdtraj.rmsd(traj, system, 0, atom_indices=heavy_atoms)
 
 #Plotting using ASH-plot (matplotlib)
 x_label="Time (ps)"
-y_label="RMSD (Å)"
+y_label="RMSD (nm)"
 filelabel="RMSD"
 eplot = ASH_plot(filelabel, num_subplots=1, x_axislabel=x_label, y_axislabel=y_label)
 eplot.addseries(0, x_list=traj.time, y_list=rmsd_heavy, label=y_label, color='blue', line=True, scatter=True)
