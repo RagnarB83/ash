0dd3925c7be5fa6a0b9734ec4371071f8d38b114
RagnarB83 <ragnar.bjornsson@gmail.com>
2024-01-25 13:57:33 +0100

    -openmm tests: skipping OpenMM_Opt for now - qmmm: pyscf only 1 core for now

--- ash/tests/test_QM_MM_openmm.py
+++ ash/tests/test_QM_MM_openmm.py
@@ -15,7 +15,7 @@ def test_qm_mm_pyscf_openmm():
             numcores=numcores, autoconstraints=None, rigidwater=False)
     #QM
     qmatomlist = [1013,1014,1015,1016,1017,1018]
-    qm = PySCFTheory(scf_type="RKS",  functional="BP86", basis="def2-SVP", densityfit=True,numcores=numcores)
+    qm = PySCFTheory(scf_type="RKS",  functional="BP86", basis="def2-SVP", densityfit=True)
     #qm = xTBTheory()
     # Create QM/MM OBJECT
     qmmmobject = QMMMTheory(qm_theory=qm, mm_theory=omm, qm_charge=-1, qm_mult=1,
--- ash/tests/test_openmm.py
+++ ash/tests/test_openmm.py
@@ -5,21 +5,19 @@ from math import isclose
 #Read solvated PDB-file, create OpenMMTheory job and run MM singlepoint
 def test_openmm_basic():
 
-    numcores=2
     #Defining fragment containing coordinates (can be read from XYZ-file, ASH fragment or PDB-file)
     pdbfile=f"{ashpath}/tests/pdbfiles/1aki_solvated.pdb"
     fragment=Fragment(pdbfile=pdbfile)
 
     #Creating new OpenMM object from OpenMM full system file
     omm = OpenMMTheory(xmlfiles=["charmm36.xml", "charmm36/water.xml"], pdbfile=pdbfile, periodic=True,
-            numcores=numcores, autoconstraints=None, rigidwater=False)
+            autoconstraints=None, rigidwater=False)
     #Singlepoint MM energy
     Singlepoint(theory=omm, fragment=fragment, Grad=True)
 
 #Read raw PDB-file, fix using pdbfixer, setup using Modeller and optimize
 def test_openmm_modeller():
 
-    numcores=2
     pdbfile=f"{ashpath}/tests/pdbfiles/1aki.pdb"
 
     #Setting up new system, adding hydrogens, solvent, ions and defining forcefield, topology
@@ -27,7 +25,7 @@ def test_openmm_modeller():
         solvent_padding=10.0, ionicstrength=0.1, platform='CPU')
 
     #MM minimization to get rid the worst contacts
-    OpenMM_Opt(fragment=ashfragment, theory=openmmobject, maxiter=100, tolerance=1000)
+    #OpenMM_Opt(fragment=ashfragment, theory=openmmobject, maxiter=100, tolerance=1000)
     
 #test_openmm_basic()
 #test_openmm_modeller()
