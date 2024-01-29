59b0b98b4496d6ec7ccc9f2129b6d429c3219011
RagnarB83 <ragnar.bjornsson@gmail.com>
2024-01-29 12:32:44 +0100

    minor cleanup

--- ash/functions/functions_general.py
+++ ash/functions/functions_general.py
@@ -47,16 +47,23 @@ def create_ash_env_file():
     ash_multiline_string=f"""
 #!/bin/bash
 
-#
+#################################
+# Setting up ASH environment
+#################################
+
 ulimit -s unlimited
+
 ASHPATH={ash_dir}
 python3path={path_to_python3_dir}
+
 #PYTHONPATH for finding ASH usually not recommended.
 #Better to install into Python environment (pip install)
 #export PYTHONPATH=$ASHPATH:\$ASHPATH/lib:$PYTHONPATH
+
 export PATH=$python3path:$PATH
 export LD_LIBRARY_PATH=$ASHPATH/lib:$LD_LIBRARY_PATH
 
+#################################
 echo "Sourced ASH environment file!"
 echo "Importing ASH within Python should now work!"
 echo "ASH is located in $ASHPATH"
