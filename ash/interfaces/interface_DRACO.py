a2f1ecde19e6120024426dc820564cb836db1804
RagnarB83 <ragnar.bjornsson@gmail.com>
2024-03-06 10:05:38 +0100

    draco fixes

--- ash/interfaces/interface_DRACO.py
+++ ash/interfaces/interface_DRACO.py
@@ -14,14 +14,22 @@ import ash.settings_ash
 # Can be used (with modifications) as input radii in ORCA and other codes
 
 
-def get_draco_radii(fragment=None, xyzfile=None, dracodir=None, radii_type='cpcm', solvent=None):
+def get_draco_radii(fragment=None, xyzfile=None, charge=None, dracodir=None, radii_type='cpcm', solvent=None,
+                    chargemodel='ceh'):
 
-    if fragment == None and xyzfile == None:
+    #Early exits
+    if fragment is None and xyzfile is None:
         print(BC.FAIL, "No fragment or xyzfile provided. Exiting.", BC.END)
         ashexit()
+
     if fragment != None:
         print("Using ASH fragment. Writing XYZ-file to disk")
         xyzfile = fragment.write_xyzfile(xyzfilename="fragment.xyz")
+        charge=fragment.charge #Setting charge from fragment
+    #Checking if charge was set (by user or from fragment)
+    if charge is None:
+        print("Error: you must provide a charge for the molecule")
+        ashexit()
 
     dracodir=check_DRACO_location(dracodir)
     print("DRACO directory:", dracodir)
@@ -29,17 +37,16 @@ def get_draco_radii(fragment=None, xyzfile=None, dracodir=None, radii_type='cpcm
     print("Radii type:", radii_type)
     print("Solvent:", solvent)
 
-
-    #draco h2o.xyz --solvent water
     print("Running draco. Will write output to draco.out")
     with open('draco.out', 'w') as ofile:
-        args_list = [dracodir+'/draco', xyzfile, '--radii', radii_type, '--solvent', solvent]
+        args_list = [dracodir+'/draco', xyzfile, '--radii', radii_type, '--solvent',
+                     solvent,'--charge', str(charge), '--chrgmodel', chargemodel]
         print("args_list:", args_list)
         sp.run(args_list, stdout=ofile)
 
     #Grab radii from draco.out
     radii = grab_radii('draco.out')
-
+    print("Found Draco radii:", radii)
     return radii
 
 def grab_radii(outfile):
