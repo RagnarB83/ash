4b68a66e10fbdb2b8ff6e2acafbfbec17a04e5e3
RagnarB83 <ragnar.bjornsson@gmail.com>
2024-04-23 13:09:13 +0200

    MNDO debugging: pc coord format

--- ash/interfaces/interface_MNDO.py
+++ ash/interfaces/interface_MNDO.py
@@ -229,8 +229,8 @@ def write_mndo_input(method,filename,elems,coords,charge,mult, PC=False, MMcharg
     if PC is True:
         pcdata = np.column_stack((MMcoords, MMcharges))
         #Fast appending to file
-        ash.functions.functions_general.fast_nparray_write(pcdata,filename=f"{filename}.inp", writemode="a")
-
+        #ash.functions.functions_general.fast_nparray_write(pcdata,filename=f"{filename}.inp", writemode="a")
+        np.savetxt(f, pcdata, fmt='%12.7f %12.7f %12.7f %8.4f')
         #Old:
         #for q,pc_c in zip(MMcharges,MMcoords):
         #    f.write(f"{pc_c[0]} {pc_c[1]} {pc_c[2]} {q}\n")
