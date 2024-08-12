628b1fb328349e4492ba3a9faccba39307f53994
RagnarB83 <ragnar.bjornsson@gmail.com>
2024-08-12 12:30:50 +0200

    fix

--- ash/interfaces/interface_ORCA.py
+++ ash/interfaces/interface_ORCA.py
@@ -3952,7 +3952,7 @@ def new_ORCA_natorbsfile_from_density(gbwfile, densityname="mdcip", result_file=
                 mol_data["HFTyp"] = "ROHF"
                 print("mol_data orblabels old", mol_data["MolecularOrbitals"]["OrbitalLabels"])
                 mol_data["MolecularOrbitals"]["OrbitalLabels"] = mol_data["MolecularOrbitals"]["OrbitalLabels"][0:int(len(mol_data["MolecularOrbitals"]["OrbitalLabels"])/2)]
-                print("mol_data orblabels new", mol_data["OrbitalLabels"])
+                print("mol_data orblabels new", mol_data["MolecularOrbitals"]["OrbitalLabels"])
                 break
     
 
