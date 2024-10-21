a0578d7b31b70f766a42db08135ae26234288426
RagnarB83 <ragnar.bjornsson@gmail.com>
2024-10-21 13:03:13 +0200

    renaming of mtd example files

--- examples/metadynamics/2CV-3FGABA/explicit-solvent/mtd.py
+++ examples/metadynamics/2CV-3FGABA/explicit-solvent/mtd.py
@@ -5,7 +5,7 @@ qmcores=1
 biasdir="/home/321.5-COMX/321.5.1-Commun/ragnar/ASH-metadynamics/3fgaba/tutorial/QM-MM/100ps-MTD-timesteps/1fs/biasdirectory"
 
 #System
-xyzfile="newfragment.xyz"
+xyzfile="system_aftersolvent.xyz"
 pdbfile="system_aftersolvent.pdb"
 frag = Fragment(xyzfile=xyzfile, charge=0, mult=1)
 qmatoms = list(range(0,16))
@@ -13,7 +13,7 @@ qmatoms = list(range(0,16))
 
 #Define QM, MM and QM/MM Theory
 qm_theory = xTBTheory(runmode='inputfile', printlevel=0, numcores=qmcores)
-mm_theory = OpenMMTheory(xmlfiles=[f"{ashpath}/databases/forcefields/tip3p_water_ions.xml", "solute.xml"], 
+mm_theory = OpenMMTheory(xmlfiles=[f"{ashpath}/databases/forcefields/tip3p_water_ions.xml", "LIG.xml"], 
     pdbfile=pdbfile, platform='CPU', rigidwater=True, printlevel=0, numcores=qmcores, periodic=True)
 qm_mm_theory = QMMMTheory(qm_theory=qm_theory, mm_theory = mm_theory, qmatoms=qmatoms, fragment=frag, printlevel=0)
 
