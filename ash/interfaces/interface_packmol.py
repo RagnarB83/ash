44bb34a85c4ae4a6e27cfc2324ef7b8573e4b5bd
madhursharmaa <s.madhur58@gmail.com>
2025-06-08 22:06:55 +0200

    Modified the packmol_interface to enable it to accurately model system with mixture of solvents

--- ash/interfaces/interface_packmol.py
+++ ash/interfaces/interface_packmol.py
@@ -1,62 +1,125 @@
 import subprocess as sp
 import os
 import shutil
-import math
 from ash.functions.functions_general import ashexit, BC,print_time_rel, print_line_with_mainheader,listdiff
 from ash.modules.module_coords import writepdb_with_connectivity
 import ash.settings_ash
 
 # Basic packmol interface
 
-def packmol_solvate(packmoldir=None, inputfiles=None, num_mols=None, tolerance=2.0, result_file="final", shape="box",
-                    min_coordinates=[0.0, 0.0, 0.0], max_coordinates=[40.0, 40.0, 40.0],density=None):
+def packmol_solvate(packmoldir=None,ligand_files=None, num_mols_ligands=None, solvent_files=None, solvents_ratio=None, tolerance=2.0, result_file="final", shape="box",
+                    min_coordinates=[0.0, 0.0, 0.0], max_coordinates=[40.0, 40.0, 40.0],total_density=None):
 
+    ligands_mass= []
+    solvents_mass= []
     packmoldir=check_packmol_location(packmoldir)
     print("packmoldir:", packmoldir)
     print()
-    if inputfiles is None :
-        print("Error: inputfiles  input arguments are required!")
-        exit()
-    if num_mols is not None :
-        if len(inputfiles) != len(num_mols):
+    if ligand_files is not None:
+        print("Ligand files:", ligand_files)
+    else:
+        print("No ligand files provided, only solvent files will be packed.")
+        num_mols_ligands = None
+
+    if num_mols_ligands is None:
+        print("Info: No number of molecules for ligands provided! Will use 1 molecule per ligand file!")
+        num_mols_ligands = [1] * len(ligand_files) if ligand_files else []
+    else:
+        if len(ligand_files) != len(num_mols_ligands):
+            print("Error: Input-list variables ligand_files and num_mols_ligands must have the same size! ")
+            ashexit()
+
+    if solvent_files is None :
+        print("Info: No solvent provided! Only Ligand files would be packed!")
+        #print("Error: inputfiles  input arguments are required!")
+        #exit()
+        solvents_ratio = None
+
+    if solvents_ratio is not None :
+        if len(solvent_files) != len(solvents_ratio):
             print("Error: Input-list variables inputfiles and num_moles must have the same size! ")
             ashexit()
-    if density is None and num_mols is None:
+    if total_density is None and solvents_ratio is None:
         print("Error: Either density or num_mols input arguments is required!")
         ashexit()
-    if density is not None and num_mols is not None:
-        print("Error: Only one of density or num_mols input arguments is required!")
-        ashexit()
+
 
     # If density is given, calculate num_mols
-    if density is not None:
-        print(f"A density of {density} g/ml was specified. Calculating number of molecules based on density and volume.")
-        if len(inputfiles) != 1:
-            print("Error: Only one input file is allowed when using density as input!")
-            ashexit()
+    if total_density is not None:
+        print(f"A density of {total_density} g/ml was specified. Calculating number of molecules based on density and volume.")
+        #if len(solvent_files) != 1:
+        #    print("Error: Only one input file is allowed when using density as input!")
+        #    ashexit()
         # read file to get mass
-        if inputfiles[0].endswith(".pdb"):
-            frag = ash.Fragment(pdbfile=inputfiles[0], use_atomnames_as_elements=True,printlevel=0)
-        elif inputfiles[0].endswith(".mol"):
-            frag = ash.Fragment(xyzfile=inputfiles[0], printlevel=0)
-        print("Molecule MW:", frag.mass, "g/mol")
+        for  ligand_file in ligand_files:
+            if ligand_file.endswith(".pdb"):
+                frag = ash.Fragment(pdbfile=ligand_file, use_atomnames_as_elements=True,printlevel=0)
+                print(f"{ligand_file} MW:", frag.mass, "g/mol")
+            elif ligand_file.endswith(".mol"):
+                frag = ash.Fragment(xyzfile=ligand_file, printlevel=0)
+                print(f"{ligand_file} MW:", frag.mass, "g/mol")
+            else:
+                print(f"Unsupported ligand file format: {ligand_file}")
+                ashexit()
+            ligands_mass.append(frag.mass)
+
+        for solvent_file in solvent_files:
+            if solvent_file.endswith(".pdb"):
+                frag = ash.Fragment(pdbfile=solvent_file, use_atomnames_as_elements=True,printlevel=0)
+                print(f"{solvent_file} MW:", frag.mass, "g/mol")
+            elif solvent_file.endswith(".mol"):
+                frag = ash.Fragment(xyzfile=solvent_file, printlevel=0)
+                print(f"{solvent_file} MW:", frag.mass, "g/mol")
+            else:
+                print(f"Unsupported solvent file format: {solvent_file}")
+                ashexit()
+            solvents_mass.append(frag.mass)
+        
         # Volumes from coordinates
         volume = (max_coordinates[0]-min_coordinates[0])*(max_coordinates[1]-min_coordinates[1])*(max_coordinates[2]-min_coordinates[2])
+        #print("Volume of box:", volume, "A^3")
+        #num_mol = density_to_num_mol(total_density, volume, frag.mass)
+        #print("Number of molecules:", num_mol)
+        #solvents_ratio=[num_mol]
+        #print(solvents_ratio)
+        Avo_N = 6.02214076E+23
+        volume_ml = 1.00E-24*volume
+        #num_mol = math.ceil((density*volume_ml*Avo_N)/MW)
+        print("Total Desity Desired:", total_density, "g/ml")
         print("Volume of box:", volume, "A^3")
-        num_mol = density_to_num_mol(density, volume, frag.mass)
-        print("Number of molecules:", num_mol)
-        num_mols=[num_mol]
-        print(num_mols)
+        print("Will compute mass of ligands and solvents and calculate number of molecules for each solvent based on ratio and total density.")
+
+        ligand_total_mass = sum([ligands_mass[i] * num_mols_ligands[i] for i in range(len(ligands_mass))])
+        print("Total mass of ligands:", ligand_total_mass, "g/mol")
+       
+        solvent_total_mass = sum([solvents_mass[i] * solvents_ratio[i] for i in range(len(solvents_mass))])
+
+        total_mass = total_density * volume_ml * Avo_N  # total mass in grams
+        print("Total mass of system:", total_mass, "g/mol")
+        # Solve for m:
+        m = (total_mass - ligand_total_mass) / solvent_total_mass
+        print("Number of molecules of each solvent to achieve desired density (for ratio 1):", m)
+        # Number of molecules of each solvent
+        solvent_molecule_counts = [int(round(m * solvents_ratio[j] )) for j in range(len(solvents_mass))]
+        print(f"Number of molecules for {solvent_files[i]}: {m} * {solvents_ratio[i]}" for i in range(len(solvents_mass)))
+
+
 
     # Determining filetype
-    filetype=os.path.splitext(inputfiles[0])[1].replace(".","")
+    filetype=os.path.splitext(solvent_files[0])[1].replace(".","")
     print("Filetype is:", filetype)
 
     # PRINT
-    print("Inputfiles:", inputfiles)
-    print("Num_mols:", num_mols)
-    for f,m in zip(inputfiles,num_mols):
+    print("Ligand Inputfiles:", ligand_files)
+    print("Num_mols Ligands:", num_mols_ligands)
+    for f,m in zip(ligand_files,num_mols_ligands):
         print(f"{f} : {m} molecules")
+
+    print("Solvent Inputfiles:", solvent_files)
+    print("Solvent Ratio:", solvents_ratio)
+    for f,m in zip(solvent_files,solvent_molecule_counts):
+        print(f"{f} : {m} molecules")
+    
 
     # WRITING INPUTFILE
     input_header=f"""
@@ -66,14 +129,37 @@ output {result_file}.{filetype}
 """
 
     input_main=""
-    for struct_file,num_mol in zip(inputfiles,num_mols):
-        temp=f"""
+    ligands_input = ""
+    solvents_input = ""
+    if len(ligand_files) == 1 and num_mols_ligands[0] == 1:
+        print("Only one ligand present, will fix it's position in the center of the box.")
+        min_coordinates_single = [(max_coordinates[i] + min_coordinates[i]) / 2 for i in range(3)]
+        max_coordinates_single = [(max_coordinates[i] + min_coordinates[i]) / 2 for i in range(3)]
+        for struct_file, num_mol in zip(ligand_files, num_mols_ligands):
+            ligands_input = f"""
+        structure {struct_file}
+        number {num_mol}
+        fixed {min_coordinates_single[0]} {min_coordinates_single[1]} {min_coordinates_single[2]} {max_coordinates_single[0]} {max_coordinates_single[1]} {max_coordinates_single[2]}
+        end structure
+        """
+    else:
+        print("Multiple ligands present, will use the provided box coordinates for packing.")
+        for struct_file, num_mol in zip(ligand_files, num_mols_ligands):
+            ligands_input += f"""
+        structure {struct_file}
+        number {num_mol}
+        fixed {min_coordinates[0]} {min_coordinates[1]} {min_coordinates[2]} {max_coordinates[0]} {max_coordinates[1]} {max_coordinates[2]}
+        end structure
+        """
+    
+    for struct_file,num_mol in zip(solvent_files,solvent_molecule_counts):
+        solvents_input +=f"""
     structure {struct_file}
     number {num_mol}
     inside {shape} {min_coordinates[0]} {min_coordinates[1]} {min_coordinates[2]} {max_coordinates[0]} {max_coordinates[1]} {max_coordinates[2]}
     end structure
     """
-        input_main= input_main+temp
+        input_main= ligands_input + solvents_input
     with open("inputfile.inp", "w") as f:
         f.write(input_header)
         f.write(input_main)
@@ -91,14 +177,14 @@ output {result_file}.{filetype}
     print("Packmol output can be found in packmol.out")
     print(f"Created file: {result_filename}")
 
-    if inputfiles[0].endswith(".pdb"):
+    if solvent_files[0].endswith(".pdb"):
         print("\nWarning: Packmol-created PDB-files may not contain correct connectivity information!")
         print("Will now try to use ASH function writepdb_with_connectivity to write a new PDB-file with connectivity information.")
         writepdb_with_connectivity("final.pdb")
         result_filename=f"{result_file}_withcon.{filetype}"
         print("Created file:", result_filename)
         print("Creating also single-molecule file:")
-        writepdb_with_connectivity(inputfiles[0])
+        writepdb_with_connectivity(solvent_files[0])
     return result_filename
 
 def check_packmol_location(packmoldir, binary_name="packmol", dirname="packmoldir"):
@@ -125,9 +211,3 @@ def check_packmol_location(packmoldir, binary_name="packmol", dirname="packmoldi
     return finaldir
 
 
-#Convert desired density (g/ml) and desired volume (A^3) to number of a given molecule
-def density_to_num_mol(density, volume, MW):
-    Avo_N = 6.02214076E+23
-    volume_ml = 1.00E-24*volume
-    num_mol = math.ceil((density*volume_ml*Avo_N)/MW)
-    return num_mol
